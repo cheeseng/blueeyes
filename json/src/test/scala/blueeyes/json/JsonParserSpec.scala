3254a02c4c7016cd952f3ec19a7101ff79b66508
cheeseng <cheeseng@amaseng.com>
2012-07-09 22:08:32 +0800

    Rewritten JsonParserSpec using ScalaTest.

--- json/src/test/scala/blueeyes/json/JsonParserSpec.scala
+++ json/src/test/scala/blueeyes/json/JsonParserSpec.scala
@@ -18,14 +18,15 @@ package blueeyes.json
 
 //import _root_.org.scalacheck._
 import _root_.org.scalacheck.Prop._
-import org.specs2.mutable.Specification
-import org.specs2.ScalaCheck
+import org.scalatest.WordSpec
+import org.scalatest.matchers.MustMatchers
+import org.scalatest.prop.Checkers
 import org.scalacheck.Gen
 import java.net.URLDecoder
 import JsonAST._
 import scala.util.control.Exception._
 
-object JsonParserSpec extends Specification with ArbitraryJValue with ScalaCheck {
+class JsonParserSpec extends WordSpec with MustMatchers with ArbitraryJValue with Checkers {
   import JsonAST._
   import JsonParser._
   import Printer._
@@ -37,8 +38,10 @@ object JsonParserSpec extends Specification with ArbitraryJValue with ScalaCheck
 
   "Buffer size does not change parsing result" in {
     val bufSize = Gen.choose(2, 64)
-    val parsing = (x: JValue, s1: Int, s2: Int) => { parseVal(x, s1) mustEqual parseVal(x, s2) }
-    forAll(genObject, bufSize, bufSize)(parsing)
+    val parsing = (x: JValue, s1: Int, s2: Int) => { parseVal(x, s1) == (parseVal(x, s2)) }
+    forAll { (genObject: JValue, bufSize1: Int, bufSize2: Int) => 
+      parsing(genObject, bufSize1, bufSize2)
+    }
   }
 
   "Parsing is thread safe" in {
@@ -48,11 +51,11 @@ object JsonParserSpec extends Specification with ArbitraryJValue with ScalaCheck
     val executor = Executors.newFixedThreadPool(100)
     val results = (0 to 100).map(_ =>
       executor.submit(new Callable[JValue] { def call = parse(json) })).toList.map(_.get)
-    results.zip(results.tail).forall(pair => pair._1 == pair._2) mustEqual true
+    results.zip(results.tail).forall(pair => pair._1 == pair._2) must equal (true)
   }
 
   "All valid string escape characters can be parsed" in {
-    parse("[\"abc\\\"\\\\\\/\\b\\f\\n\\r\\t\\u00a0\\uffff\"]") must_== JArray(JString("abc\"\\/\b\f\n\r\t\u00a0\uffff")::Nil)
+    parse("[\"abc\\\"\\\\\\/\\b\\f\\n\\r\\t\\u00a0\\uffff\"]") must equal (JArray(JString("abc\"\\/\b\f\n\r\t\u00a0\uffff")::Nil))
   }
 
   private def parseVal(json: JValue, bufSize: Int) = {
@@ -67,20 +70,20 @@ object JsonParserSpec extends Specification with ArbitraryJValue with ScalaCheck
   }
 }
 
-object ParserBugs extends Specification {
+class ParserBugs extends WordSpec with MustMatchers {
   "Unicode ffff is a valid char in string literal" in {
-    JsonParser.parseOpt(""" {"x":"\uffff"} """).isDefined mustEqual true
+    JsonParser.parseOpt(""" {"x":"\uffff"} """).isDefined must equal (true)
   }
 
   "Does not hang when parsing 2.2250738585072012e-308" in {
-    allCatch.opt(JsonParser.parse(""" [ 2.2250738585072012e-308 ] """)) mustEqual None
+    allCatch.opt(JsonParser.parse(""" [ 2.2250738585072012e-308 ] """)) must equal (None)
   }
 
   "Does not hang when parsing 22.250738585072012e-309" in {
-    allCatch.opt(JsonParser.parse(""" [ 22.250738585072012e-309 ] """)) mustEqual None
+    allCatch.opt(JsonParser.parse(""" [ 22.250738585072012e-309 ] """)) must equal (None)
   }
 
   "Can parse funky characters" in {
-    JsonParser.parse(URLDecoder.decode("\"%E2%84%A2\"", "UTF-8")) must_== JString("™")
+    JsonParser.parse(URLDecoder.decode("\"%E2%84%A2\"", "UTF-8")) must equal (JString("™"))
   }
 }
