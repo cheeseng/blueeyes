ceaea6020c74c52be0eaa2c3d9a50ba029e41d4b
cheeseng <cheeseng@amaseng.com>
2012-07-11 15:51:54 +0800

    Rewritten BijectionsChunkJsonSpec using ScalaTest.

--- core/src/test/scala/blueeyes/core/data/BijectionsChunkJsonSpec.scala
+++ core/src/test/scala/blueeyes/core/data/BijectionsChunkJsonSpec.scala
@@ -1,16 +1,19 @@
 package blueeyes.core.data
 
-import org.specs2.mutable.Specification
+import org.scalatest.WordSpec
+import org.scalatest.matchers.MustMatchers
 import blueeyes.json.JsonAST._
 import blueeyes.json.JsonParser.ParseException
 
-class BijectionsChunkJsonSpec extends Specification with BijectionsChunkJson{
+class BijectionsChunkJsonSpec extends WordSpec with MustMatchers with BijectionsChunkJson{
   "BijectionsChunkJson" should{
     "parser valid JSON" in{
-      JValueToChunk.unapply(Chunk("""{"foo": "bar"}""".getBytes())) mustEqual(JObject(List(JField("foo", JString("bar")))))
+      JValueToChunk.unapply(Chunk("""{"foo": "bar"}""".getBytes())) must equal (JObject(List(JField("foo", JString("bar")))))
     }
     "throw error when JSON is incomplete" in{
-      JValueToChunk.unapply(Chunk("""{"foo": "bar""".getBytes())) must throwA[ParseException]
+      intercept[ParseException] {
+        JValueToChunk.unapply(Chunk("""{"foo": "bar""".getBytes()))
+      }
     }
   }
 }
