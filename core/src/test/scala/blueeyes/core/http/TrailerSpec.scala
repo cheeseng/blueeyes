928fcfe619a684d3b89367250826bd0ff8a5e300
cheeseng <cheeseng@amaseng.com>
2012-07-12 13:54:41 +0800

    Rewritten TrailerSpec using ScalaTest.

--- core/src/test/scala/blueeyes/core/http/TrailerSpec.scala
+++ core/src/test/scala/blueeyes/core/http/TrailerSpec.scala
@@ -1,15 +1,16 @@
 package blueeyes.core.http
 
-import org.specs2.mutable.Specification
+import org.scalatest.WordSpec
+import org.scalatest.matchers.MustMatchers
 
-class TrailerSpec extends Specification{
+class TrailerSpec extends WordSpec with MustMatchers {
 
   "Trailer/HttpHeaderFields: Should parse correctly, if parsing for trailer" in {
-    HttpHeaders.Trailer(HttpHeaderField.parseAll("Accept, Age, Date, Max-Forwards, Content-Length", "trailer"): _*).value mustEqual "Accept, Age, Date, Max-Forwards"
+    HttpHeaders.Trailer(HttpHeaderField.parseAll("Accept, Age, Date, Max-Forwards, Content-Length", "trailer"): _*).value must equal ("Accept, Age, Date, Max-Forwards")
   }
 
   "Trailer/HttpHeaderFields: Should also prarse correctly if not parsing fo the trailer" in {
-    HttpHeaderField.parseAll("Accept, Age, Date, Max-Forwards, Cats, Content-Length", "").map(_.toString).mkString(", ") mustEqual "Accept, Age, Date, Max-Forwards, Content-Length"
+    HttpHeaderField.parseAll("Accept, Age, Date, Max-Forwards, Cats, Content-Length", "").map(_.toString).mkString(", ") must equal ("Accept, Age, Date, Max-Forwards, Content-Length")
   }
 
 }
