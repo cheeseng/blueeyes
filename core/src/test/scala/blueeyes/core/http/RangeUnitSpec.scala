031eb2b204a86e912cfe09feeffd1ca81e9a7c7f
cheeseng <cheeseng@amaseng.com>
2012-07-12 13:48:44 +0800

    Rewritten RangeUnitSpec using ScalaTest.

--- core/src/test/scala/blueeyes/core/http/RangeUnitSpec.scala
+++ core/src/test/scala/blueeyes/core/http/RangeUnitSpec.scala
@@ -1,15 +1,16 @@
 package blueeyes.core.http
 
-import org.specs2.mutable.Specification
+import org.scalatest.WordSpec
+import org.scalatest.matchers.MustMatchers
 
-class RangeUnitSpec extends Specification{
+class RangeUnitSpec extends WordSpec with MustMatchers {
 
   "Range-Units:  Should parse \"bytes\" as Some(bytes) produce None for \"cats\"" in {
-    RangeUnits.parseRangeUnits("bytes").map(_.toString).getOrElse("") mustEqual "bytes"
-    RangeUnits.parseRangeUnits("cats").map(_.toString).getOrElse("") mustEqual ""
+    RangeUnits.parseRangeUnits("bytes").map(_.toString).getOrElse("") must equal ("bytes")
+    RangeUnits.parseRangeUnits("cats").map(_.toString).getOrElse("") must equal ("")
   }
 
   "Accept-Ranges:  Should create none from \"none\"" in {
-    HttpHeaders.`Accept-Ranges`(RangeUnits.parseRangeUnits("none").get).value mustEqual "none"
+    HttpHeaders.`Accept-Ranges`(RangeUnits.parseRangeUnits("none").get).value must equal ("none")
   }
 }
