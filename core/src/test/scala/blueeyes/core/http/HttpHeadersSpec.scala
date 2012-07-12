fb1a6d6105ac3bc3a2e153a86007367386471e14
cheeseng <cheeseng@amaseng.com>
2012-07-12 12:57:41 +0800

    Rewritten HttpHeadersSpec using ScalaTest.

--- core/src/test/scala/blueeyes/core/http/HttpHeadersSpec.scala
+++ core/src/test/scala/blueeyes/core/http/HttpHeadersSpec.scala
@@ -1,30 +1,31 @@
 package blueeyes.core.http
 
-import org.specs2.mutable.Specification
+import org.scalatest.WordSpec
+import org.scalatest.matchers.MustMatchers
 import HttpHeaders._
 
-class HttpHeadersSpec extends Specification{
+class HttpHeadersSpec extends WordSpec with MustMatchers {
   "HttpHeaders" should {
     "find headers by type" in{
       val headers = HttpHeaders(List("authorization" -> "foo"))
-      headers.header[Authorization] must beSome(Authorization("foo"))
+      headers.header[Authorization] must be (Some(Authorization("foo")))
     }
 
     "extract known header types" in {
       val headers = Map("authorization" -> "foo")
 
       val auths = for (Authorization(auth) <- headers) yield auth
-      auths must_== List("foo") 
+      auths must equal (List("foo")) 
     }
     "extract Host header types without scheme" in {
       val headers = Map("Host" -> "localhost:8585")
 
       val hosts = for (Host(host) <- headers) yield host
-      hosts.map(Host(_).value) must_== List("localhost:8585")
+      hosts.map(Host(_).value) must equal (List("localhost:8585"))
     }
 
     "parse custom tuples" in {
-      HttpHeader(("Blargh" -> "foo")) must_== CustomHeader("Blargh", "foo")
+      HttpHeader(("Blargh" -> "foo")) must equal (CustomHeader("Blargh", "foo"))
     }
   }
 }
