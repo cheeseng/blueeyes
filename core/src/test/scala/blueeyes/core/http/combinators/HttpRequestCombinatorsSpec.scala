5821656607f05afe969bdf40e90e1326f4bd182b
cheeseng <cheeseng@amaseng.com>
2012-07-12 16:15:41 +0800

    Changed HttpRequestCombinatorsSpec to use HttpRequestCheckers.

--- core/src/test/scala/blueeyes/core/http/combinators/HttpRequestCombinatorsSpec.scala
+++ core/src/test/scala/blueeyes/core/http/combinators/HttpRequestCombinatorsSpec.scala
@@ -6,28 +6,19 @@ import org.scalatest.matchers.MustMatchers
 import blueeyes.core.http.{HttpRequest, HttpResponse, HttpException, HttpStatus}
 import blueeyes.core.http.HttpStatusCodes._
 import blueeyes.core.http.HttpMethods._
-import blueeyes.core.http.test.HttpRequestMatchers
 import blueeyes.json.JsonAST._
 import akka.dispatch.Future
-import blueeyes.concurrent.test.AkkaFutures
+import blueeyes.core.http.test.HttpRequestCheckers
 
-class HttpRequestCombinatorsSpec extends WordSpec with MustMatchers with HttpRequestCombinators with AkkaFutures with blueeyes.bkka.AkkaDefaults {
+class HttpRequestCombinatorsSpec extends WordSpec with MustMatchers with HttpRequestCombinators with HttpRequestCheckers with blueeyes.bkka.AkkaDefaults {
   type Handler[T, S] = HttpRequest[Future[T]] => Future[HttpResponse[S]]
   
   "refineContentType should return bad request type cannot be refined to specified subtype" in {
-    jObjectCaller { refineContentType { jIntHandler } }.futureValue match {
-      case HttpResponse(status, _, _, _) => status.code must equal (BadRequest)
-      case other => fail("Expected BadRequest, but got " + other)
-    }
+    respondWithCode(jObjectCaller { refineContentType { jIntHandler } }, BadRequest)
   }
   
   "refineContentType should refine content type when possible" in {
-    jIntCaller { refineContentType { jIntHandler } }.futureValue match {
-      case HttpResponse(status, _, Some(content), _) => 
-        status.code must equal (OK)
-        content must equal (JInt(123))
-      case other => fail("Expected OK Request with content, but got " + other)
-    }
+    succeedWithContent(jIntCaller { refineContentType { jIntHandler } }, JInt(123))
   }
   
   def jObjectCaller(h: Handler[JValue, JValue]) = h(HttpRequest(uri = "/", method = GET, content = Some(Future(JObject(Nil)))))
