e2bc25c34227d13a3891d3b32e8c4c6e2efccf68
cheeseng <cheeseng@amaseng.com>
2012-07-12 13:36:22 +0800

    Rewritten LanguageRangeSpec using ScalaTest.

--- core/src/test/scala/blueeyes/core/http/LanguageRangeSpec.scala
+++ core/src/test/scala/blueeyes/core/http/LanguageRangeSpec.scala
@@ -1,16 +1,17 @@
 package blueeyes.core.http
 
-import org.specs2.mutable.Specification
+import org.scalatest.WordSpec
+import org.scalatest.matchers.MustMatchers
 
-class LanguageRangeSpec extends Specification{
+class LanguageRangeSpec extends WordSpec with MustMatchers {
 
   "Language-Range:  Should produce en-uk from \"en-uk\"" in {
-    LanguageRanges.parseLanguageRanges("en-uk")(0).value mustEqual "en-uk"
+    LanguageRanges.parseLanguageRanges("en-uk")(0).value must equal ("en-uk")
   }
 
   "Accept-Language:  Should create languages (en-us-calif, is, cn) from \"en-us-calif, is=now!, cn; q=1\""  in {
     HttpHeaders.`Accept-Language`(LanguageRanges.parseLanguageRanges("en-us-calif, is=now!, cn; q=1"): _*)
-      .value mustEqual "en-us-calif, is, cn"
+      .value must equal ("en-us-calif, is, cn")
   }
 
 }
