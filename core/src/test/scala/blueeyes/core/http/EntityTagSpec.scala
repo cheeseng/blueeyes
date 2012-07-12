d18eaa3674c151c9176c1ea43618d97e148c5488
cheeseng <cheeseng@amaseng.com>
2012-07-12 12:42:16 +0800

    Rewritten EntityTagSpec using ScalaTest.

--- core/src/test/scala/blueeyes/core/http/EntityTagSpec.scala
+++ core/src/test/scala/blueeyes/core/http/EntityTagSpec.scala
@@ -1,21 +1,22 @@
 package blueeyes.core.http
 
-import org.specs2.mutable.Specification
+import org.scalatest.WordSpec
+import org.scalatest.matchers.MustMatchers
 import blueeyes.core.http.MimeTypes._
 
-class EntityTagSpec extends Specification{
+class EntityTagSpec extends WordSpec with MustMatchers {
 
   /* Some more work should be done on entity tags */
   "If-Match:  Should return strings on well-formed input" in {
-    HttpHeaders.`If-Match`(EntityTags.parseEntityTags("\"c4tattack\", \"cyberTiger\"").get).value mustEqual "\"c4tattack\", \"cybertiger\""
+    HttpHeaders.`If-Match`(EntityTags.parseEntityTags("\"c4tattack\", \"cyberTiger\"").get).value must equal ("\"c4tattack\", \"cybertiger\"")
     }
 
   "If-Match:  Should return * string on presence of *; also, parser" in {
-    HttpHeaders.`If-Match`(EntityTags.parseEntityTags("*, \"c4tattack\", \"cyberTiger\"").get).value mustEqual "*"
+    HttpHeaders.`If-Match`(EntityTags.parseEntityTags("*, \"c4tattack\", \"cyberTiger\"").get).value must equal ("*")
   }
 
   "If-Match: Should return none text not enclosed with quotes" in {
-    EntityTags.parseEntityTags("w%015") mustEqual None
+    EntityTags.parseEntityTags("w%015") must equal (None)
   }
 
 }
