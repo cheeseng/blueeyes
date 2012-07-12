e51a04a32fb1c8f414dca1ff66982f330bbe3d60
cheeseng <cheeseng@amaseng.com>
2012-07-12 13:10:24 +0800

    Rewritten HttpRequestSpec using ScalaTest.

--- core/src/test/scala/blueeyes/core/http/HttpRequestSpec.scala
+++ core/src/test/scala/blueeyes/core/http/HttpRequestSpec.scala
@@ -1,45 +1,46 @@
 package blueeyes.core.http
 
-import org.specs2.mutable.Specification
+import org.scalatest.WordSpec
+import org.scalatest.matchers.MustMatchers
 
-class HttpRequestSpec extends Specification{
+class HttpRequestSpec extends WordSpec with MustMatchers {
   "HttpRequest.apply" should {
     "parse query string properly" in {
       val ps = HttpRequest(method = HttpMethods.GET, uri = "http://foo.com?a=b&c=d&e").parameters
 
-      ps mustEqual Map(
+      ps must equal (Map(
         'a -> "b",
         'c -> "d",
         'e -> ""
-      )
+      ))
     }
 
     "override parameters with query string parameters" in {
       val ps = HttpRequest(method = HttpMethods.GET, uri = "http://foo.com?a=b", parameters = Map('a -> "z")).parameters
 
-      ps mustEqual Map(
+      ps must equal (Map(
         'a -> "b"
-      )
+      ))
     }
 
     "not doubly unescape query parameters" in {
       val ps = HttpRequest(method = HttpMethods.GET, uri = "http://foo.com?a=a%26b%26c%26d").parameters
 
-      ps mustEqual Map(
+      ps must equal (Map(
         'a -> "a&b&c&d"
-      )
+      ))
     }
 
     "allow no query string" in {
       val ps = HttpRequest(method = HttpMethods.GET, uri = "http://foo.com").parameters
 
-      ps mustEqual Map.empty[Symbol, String]
+      ps must equal (Map.empty[Symbol, String])
     }
 
     "allow empty query string" in {
       val ps = HttpRequest(method = HttpMethods.GET, uri = "http://foo.com?").parameters
 
-      ps mustEqual Map.empty[Symbol, String]
+      ps must equal (Map.empty[Symbol, String])
     }
   }
 }
