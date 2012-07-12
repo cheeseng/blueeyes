46867d1330d1d0ed377cf28fcc9f6d2aba029a60
cheeseng <cheeseng@amaseng.com>
2012-07-12 13:18:01 +0800

    Rewritten HttpUriSpec using ScalaTest.

--- core/src/test/scala/blueeyes/core/http/HttpUriSpec.scala
+++ core/src/test/scala/blueeyes/core/http/HttpUriSpec.scala
@@ -1,21 +1,22 @@
 package blueeyes.core.http
 
-import org.specs2.mutable.Specification
+import org.scalatest.WordSpec
+import org.scalatest.matchers.MustMatchers
 
-class HttpUriSpec extends Specification{
+class HttpUriSpec extends WordSpec with MustMatchers {
   "Host:  Should parse correct host uri" in {
-    HttpHeaders.Host(URI.opt("http://www.socialmedia.com/coolServer/index.html").get).value mustEqual "www.socialmedia.com"
+    HttpHeaders.Host(URI.opt("http://www.socialmedia.com/coolServer/index.html").get).value must equal ("www.socialmedia.com")
   }
 
-  "Host:  Should parse correct host uri" in {
-    HttpHeaders.Host(URI.opt("http://maps.google.com/coolmap.html").get).value mustEqual "maps.google.com"
+  "Host:  Should parse correct host uri also" in {
+    HttpHeaders.Host(URI.opt("http://maps.google.com/coolmap.html").get).value must equal ("maps.google.com")
   }
 
   "Host:  Should parse correct host uri with port" in {
-    URI.opt("http://maps.google.com:8080/coolmap.html").get mustEqual URI("http://maps.google.com:8080/coolmap.html")
+    URI.opt("http://maps.google.com:8080/coolmap.html").get must equal (URI("http://maps.google.com:8080/coolmap.html"))
   }
 
   "Location: Should return correct url on parsed input" in {
-    HttpHeaders.`Location`(URI.opt("  http://www.socialmedia.com/index.html  ").get).value mustEqual "http://www.socialmedia.com/index.html"
+    HttpHeaders.`Location`(URI.opt("  http://www.socialmedia.com/index.html  ").get).value must equal ("http://www.socialmedia.com/index.html")
   }
 }
