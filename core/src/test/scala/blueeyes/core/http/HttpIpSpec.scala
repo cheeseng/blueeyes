540194a2bbd222dac262ac6fb781440ba28a9d0d
cheeseng <cheeseng@amaseng.com>
2012-07-12 13:01:26 +0800

    Rewritten HttpIpSpec using ScalaTest.

--- core/src/test/scala/blueeyes/core/http/HttpIpSpec.scala
+++ core/src/test/scala/blueeyes/core/http/HttpIpSpec.scala
@@ -1,14 +1,15 @@
 package blueeyes.core.http
 
-import org.specs2.mutable.Specification
+import org.scalatest.WordSpec
+import org.scalatest.matchers.MustMatchers
 
-class HttpIpSpec extends Specification{
+class HttpIpSpec extends WordSpec with MustMatchers {
 
   "HttpIp: Should recover the ip from \"111.11.11.1\" "in {
-    HttpIps.parseHttpIps("111.11.11.1")(0).toString mustEqual "111.11.11.1"
+    HttpIps.parseHttpIps("111.11.11.1")(0).toString must equal ("111.11.11.1")
   }
 
   "X-Forwarded-For: Should recover the relevant ips from \"111.11.11.1, 244.11.223.1, cat\" " in {
-    HttpHeaders.`X-Forwarded-For`(HttpIps.parseHttpIps("111.11.11.1, 244.11.233.1, cat"): _*).value mustEqual "111.11.11.1, 244.11.233.1"
+    HttpHeaders.`X-Forwarded-For`(HttpIps.parseHttpIps("111.11.11.1, 244.11.233.1, cat"): _*).value must equal ("111.11.11.1, 244.11.233.1")
   }
 }
