331423157d5e05973b2e3627399144d2b02ed7a6
cheeseng <cheeseng@amaseng.com>
2012-07-12 10:20:32 +0800

    Rewritten ConnectionTokenSpec using ScalaTest.

--- core/src/test/scala/blueeyes/core/http/ConnectionTokenSpec.scala
+++ core/src/test/scala/blueeyes/core/http/ConnectionTokenSpec.scala
@@ -1,17 +1,18 @@
 package blueeyes.core.http
 
-import org.specs2.mutable.Specification
+import org.scalatest.WordSpec
+import org.scalatest.matchers.MustMatchers
 import blueeyes.core.http.MimeTypes._
 
-class ConnectionTokenSpec extends Specification{
+class ConnectionTokenSpec extends WordSpec with MustMatchers {
 
   "Connection:  Should return \"foo\" when passeed \" foo 0r91j2 \\n\"." in {
-    HttpHeaders.Connection(ConnectionTokens.parseConnectionTokens(" foo 0r91j2\n ").get).value mustEqual "foo"
+    HttpHeaders.Connection(ConnectionTokens.parseConnectionTokens(" foo 0r91j2\n ").get).value must equal ("foo")
   }
   "Connection:  Should create a new connection header from \"close\"." in {
-    HttpHeaders.Connection(ConnectionTokens.parseConnectionTokens("close").get).value mustEqual "close"
+    HttpHeaders.Connection(ConnectionTokens.parseConnectionTokens("close").get).value must equal ("close")
   }
 
-  "Connection:  Should create a custom connection header from the depreciated \" Keep-Alive \"." in {    HttpHeaders.Connection(ConnectionTokens.parseConnectionTokens(" Keep-Alive ").get).value mustEqual "Keep-Alive"
+  "Connection:  Should create a custom connection header from the depreciated \" Keep-Alive \"." in {    HttpHeaders.Connection(ConnectionTokens.parseConnectionTokens(" Keep-Alive ").get).value must equal ("Keep-Alive")
   }
 }
