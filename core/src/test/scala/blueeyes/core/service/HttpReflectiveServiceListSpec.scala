392e550a9aa763f66cc37f96a66ad0efdecfddc8
cheeseng <cheeseng@amaseng.com>
2012-07-12 16:47:35 +0800

    Rewritten HttpReflectiveServiceListSpec using ScalaTest.

--- core/src/test/scala/blueeyes/core/service/HttpReflectiveServiceListSpec.scala
+++ core/src/test/scala/blueeyes/core/service/HttpReflectiveServiceListSpec.scala
@@ -1,22 +1,23 @@
 package blueeyes.core.service
 
-import org.specs2.mutable.Specification
+import org.scalatest.WordSpec
+import org.scalatest.matchers.MustMatchers
 
-class HttpReflectiveServiceListSpec extends Specification{
+class HttpReflectiveServiceListSpec extends WordSpec with MustMatchers {
 
   "HttpReflectiveServiceList: finds service if it is declared as varuable" in{
     val serviceList = new HttpReflectiveServiceList[Unit]{
       val service = ServiceImpl
     }
 
-    serviceList.services mustEqual(serviceList.service :: Nil)
+    serviceList.services must equal (serviceList.service :: Nil)
   }
   "HttpReflectiveServiceList: finds service if it is declared as methods" in{
     val serviceList = new HttpReflectiveServiceList[Unit]{
       def service = ServiceImpl
     }
 
-    serviceList.services mustEqual(serviceList.service :: Nil)
+    serviceList.services must equal (serviceList.service :: Nil)
   }
 
   object ServiceImpl extends Service[Unit]{
