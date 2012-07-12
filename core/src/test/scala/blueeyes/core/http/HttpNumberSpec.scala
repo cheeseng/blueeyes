ea60d2c0c864433f4899bee016604a2e510e8634
cheeseng <cheeseng@amaseng.com>
2012-07-12 13:05:24 +0800

    Rewritten HttpNumberSpec using ScalaTest.

--- core/src/test/scala/blueeyes/core/http/HttpNumberSpec.scala
+++ core/src/test/scala/blueeyes/core/http/HttpNumberSpec.scala
@@ -1,10 +1,11 @@
 package blueeyes.core.http
 
-import org.specs2.mutable.Specification
+import org.scalatest.WordSpec
+import org.scalatest.matchers.MustMatchers
 
-class HttpNumberSpec extends Specification{
+class HttpNumberSpec extends WordSpec with MustMatchers {
 
   "HttpNumbers:  Should return ContentLength or parse to None on bad input" in {
-    HttpNumbers.parseHttpNumbers("bees") mustEqual None
+    HttpNumbers.parseHttpNumbers("bees") must equal (None)
   }
 }
