b4c67d18b9bd363568ddc7869e531be42f1d87cb
cheeseng <cheeseng@amaseng.com>
2012-07-12 10:10:39 +0800

    Rewritten CacheDirectiveSpec using ScalaTest.

--- core/src/test/scala/blueeyes/core/http/CacheDirectiveSpec.scala
+++ core/src/test/scala/blueeyes/core/http/CacheDirectiveSpec.scala
@@ -1,22 +1,23 @@
 package blueeyes.core.http
 
-import org.specs2.mutable.Specification
+import org.scalatest.WordSpec
+import org.scalatest.matchers.MustMatchers
 
-class CacheDirectiveSpec extends Specification{
+class CacheDirectiveSpec extends WordSpec with MustMatchers {
 
   "Cache-Directive: Should parse a cache directive with a field correctly" in {
     val testString1 = "private=\"this\", no-cache, max-age=10, no-transform"
-    HttpHeaders.`Cache-Control`(CacheDirectives.parseCacheDirectives(testString1): _*).value mustEqual testString1
+    HttpHeaders.`Cache-Control`(CacheDirectives.parseCacheDirectives(testString1): _*).value must equal (testString1)
   }  
 
   "Cache-Directive: Should parse a cache-directive with a delta " in {
     val testString2 = "private, no-cache, max-stale=590, no-transform"
-    HttpHeaders.`Cache-Control`(CacheDirectives.parseCacheDirectives(testString2): _*).value mustEqual testString2
+    HttpHeaders.`Cache-Control`(CacheDirectives.parseCacheDirectives(testString2): _*).value must equal (testString2)
   }
 
   "Cache-Directive: Should return empty array on bad input" in {
     val testString3 = "amnamzimmeram"
-    CacheDirectives.parseCacheDirectives(testString3).length mustEqual 0
+    CacheDirectives.parseCacheDirectives(testString3).length must equal (0)
   }
 
 }
