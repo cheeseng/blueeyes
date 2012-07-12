8eb93d5f308e1900caa380a83961f8720ee9be8d
cheeseng <cheeseng@amaseng.com>
2012-07-12 18:24:55 +0800

    Rewritten HttpRequestLoggerJsonFormatterSpec using ScalaTest.

--- core/src/test/scala/blueeyes/core/service/HttpRequestLoggerJsonFormatterSpec.scala
+++ core/src/test/scala/blueeyes/core/service/HttpRequestLoggerJsonFormatterSpec.scala
@@ -1,28 +1,29 @@
 package blueeyes.core.service
 
-import org.specs2.mutable.Specification
+import org.scalatest.WordSpec
+import org.scalatest.matchers.MustMatchers
 import blueeyes.json.Printer
 import blueeyes.json.JsonAST._
 import blueeyes.parsers.W3ExtendedLogAST._
 
-class HttpRequestLoggerJsonFormatterSpec extends Specification{
+class HttpRequestLoggerJsonFormatterSpec extends WordSpec with MustMatchers {
   private val formatter = new HttpRequestLoggerJsonFormatter()
   "HttpRequestLoggerJsonFormatter" should{
     "create valid json for values without prefix" in{
       val values: List[(FieldIdentifier, Either[String, Array[Byte]])] = (TimeIdentifier, Left("11:11:2001")) :: (CachedIdentifier, Left("1")) :: Nil
-      formatter.formatLog(values) must_==(format(fields(values), Nil, Nil))
+      formatter.formatLog(values) must equal (format(fields(values), Nil, Nil))
     }
     "create valid json for values with client prfix" in{
       val values: List[(FieldIdentifier, Either[String, Array[Byte]])] = (MethodIdentifier(ClientToServerPrefix), Left("GET")) :: (UriIdentifier(ClientToServerPrefix), Left("/foo/bar")) :: Nil
-      formatter.formatLog(values) must_==(format(Nil, fields(values), Nil))
+      formatter.formatLog(values) must equal (format(Nil, fields(values), Nil))
     }
     "create valid json for values with server prfix for binary data" in{
       val values: List[(FieldIdentifier, Either[String, Array[Byte]])] = (ContentIdentifier(ServerToClientPrefix), Right("content".getBytes("UTF-8"))) :: Nil
-      formatter.formatLog(values) must_==(format(Nil, Nil, fields(values)))
+      formatter.formatLog(values) must equal (format(Nil, Nil, fields(values)))
     }
     "create valid json for values with server prfix" in{
       val values: List[(FieldIdentifier, Either[String, Array[Byte]])] = (StatusIdentifier(ServerToClientPrefix), Left("OK")) :: (UriIdentifier(ServerToClientPrefix), Left("/foo/bar")) :: Nil
-      formatter.formatLog(values) must_==(format(Nil, Nil, fields(values)))
+      formatter.formatLog(values) must equal (format(Nil, Nil, fields(values)))
     }
   }
   private def format(noPrefix: List[JField], clientPrefix: List[JField], serverPrefix: List[JField]) = {
