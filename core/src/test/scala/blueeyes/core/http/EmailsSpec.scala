f11d87e8168ca42e8d5c3e94192115020e0aa2d6
cheeseng <cheeseng@amaseng.com>
2012-07-12 12:34:23 +0800

    Rewritten EmailsSpec using ScalaTest.

--- core/src/test/scala/blueeyes/core/http/EmailsSpec.scala
+++ core/src/test/scala/blueeyes/core/http/EmailsSpec.scala
@@ -1,19 +1,20 @@
 package blueeyes.core.http
 
-import org.specs2.mutable.Specification
+import org.scalatest.WordSpec
+import org.scalatest.matchers.MustMatchers
 
-class EmailsSpec extends Specification{
+class EmailsSpec extends WordSpec with MustMatchers {
   "Emails" should{
     "return the correct email name with a well-formed email" in {
-      Emails("johnsmith@socialmedia.com ") must beSome(Email("johnsmith@socialmedia.com"))
+      Emails("johnsmith@socialmedia.com ") must be (Some(Email("johnsmith@socialmedia.com")))
     }
 
     "return the correct (although weird) email" in {
-      Emails(" j.o.n.Sm.ith@so.cia.lmedia.com ") must beSome(Email("j.o.n.Sm.ith@so.cia.lmedia.com"))
+      Emails(" j.o.n.Sm.ith@so.cia.lmedia.com ") must be (Some(Email("j.o.n.Sm.ith@so.cia.lmedia.com")))
     }
 
     "parse non-email to None" in {
-      Emails("209h3094)(it092jom") mustEqual None
+      Emails("209h3094)(it092jom") must equal (None)
     }
   }
 
