24adb1961619adbe49d5e1e132bfd3d4758a2f79
cheeseng <cheeseng@amaseng.com>
2012-07-12 13:27:32 +0800

    Rewritten IfRangeSpec using ScalaTest.

--- core/src/test/scala/blueeyes/core/http/IfRangeSpec.scala
+++ core/src/test/scala/blueeyes/core/http/IfRangeSpec.scala
@@ -1,15 +1,16 @@
 package blueeyes.core.http
 
-import org.specs2.mutable.Specification
+import org.scalatest.WordSpec
+import org.scalatest.matchers.MustMatchers
 
-class IfRangeSpec extends Specification{
+class IfRangeSpec extends WordSpec with MustMatchers {
 
   "If-Range:  should return an HttpDateTime from an HttpDateTime input" in {
-    HttpHeaders.`If-Range`(IfRanges.parseIfRanges("Tue, 29 Dec 2009 12:12:12 GMT").get).value mustEqual "Tue, 29 Dec 2009 12:12:12 GMT"
+    HttpHeaders.`If-Range`(IfRanges.parseIfRanges("Tue, 29 Dec 2009 12:12:12 GMT").get).value must equal ("Tue, 29 Dec 2009 12:12:12 GMT")
   }
 
-  "If-Range:  should return an HttpDateTime from an HttpDateTime input" in {
-    HttpHeaders.`If-Range`(IfRanges.parseIfRanges("\"e-tag content\"").get).value mustEqual "\"e-tag content\""
+  "If-Range:  should return an HttpDateTime from an HttpDateTime input also " in {
+    HttpHeaders.`If-Range`(IfRanges.parseIfRanges("\"e-tag content\"").get).value must equal ("\"e-tag content\"")
   }
 
 }
