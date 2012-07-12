648eb5d8b70ded8f21242b32466b2e74b160a445
cheeseng <cheeseng@amaseng.com>
2012-07-12 10:23:10 +0800

    Rewritten ContentRangeSpec using ScalaTest.

--- core/src/test/scala/blueeyes/core/http/ContentRangeSpec.scala
+++ core/src/test/scala/blueeyes/core/http/ContentRangeSpec.scala
@@ -1,15 +1,16 @@
 package blueeyes.core.http
 
-import org.specs2.mutable.Specification
+import org.scalatest.WordSpec
+import org.scalatest.matchers.MustMatchers
 
-class ContentRangeSpec extends Specification{
+class ContentRangeSpec extends WordSpec with MustMatchers {
 
   "Content-Range: Should parse bytes=1234-5678/1212 correctly" in {
-    HttpHeaders.`Content-Range`(ContentByteRanges.parseContentByteRanges("bytes=1234-5678/1212").get).value mustEqual "bytes=1234-5678/1212"
+    HttpHeaders.`Content-Range`(ContentByteRanges.parseContentByteRanges("bytes=1234-5678/1212").get).value must equal ("bytes=1234-5678/1212")
   }
 
   "Content-Range: Should parse bytes=1234-5678 to None"  in {
-    ContentByteRanges.parseContentByteRanges("bleh=1234-5678") mustEqual None
+    ContentByteRanges.parseContentByteRanges("bleh=1234-5678") must equal (None)
   }
 
 }
