a3a7cc047765668d9f23d7ae968013add1b6415a
cheeseng <cheeseng@amaseng.com>
2012-07-12 12:53:58 +0800

    Rewritten HttpDateTimeSpec using ScalaTest.

--- core/src/test/scala/blueeyes/core/http/HttpDateTimeSpec.scala
+++ core/src/test/scala/blueeyes/core/http/HttpDateTimeSpec.scala
@@ -1,19 +1,20 @@
 package blueeyes.core.http
 
-import org.specs2.mutable.Specification
+import org.scalatest.WordSpec
+import org.scalatest.matchers.MustMatchers
 
-class HttpDateTimeSpec extends Specification{
+class HttpDateTimeSpec extends WordSpec with MustMatchers {
 
   "Date:  Should return an HttpDate object with correct inputs" in {
-    HttpHeaders.Date(HttpDateTimes.parseHttpDateTimes("  MON, 01-JAN-2001 00:00:00 UTC  ").get).value mustEqual "Mon, 01 Jan 2001 00:00:00 GMT"
+    HttpHeaders.Date(HttpDateTimes.parseHttpDateTimes("  MON, 01-JAN-2001 00:00:00 UTC  ").get).value must equal ("Mon, 01 Jan 2001 00:00:00 GMT")
   }
   
   "Date: Should return an HttpDate object given correct inputs with suspect capitalization" in {
-    HttpHeaders.Date(HttpDateTimes.parseHttpDateTimes("tue, 29 dec 2009 12:12:12 GMT  ").get).value mustEqual "Tue, 29 Dec 2009 12:12:12 GMT"
+    HttpHeaders.Date(HttpDateTimes.parseHttpDateTimes("tue, 29 dec 2009 12:12:12 GMT  ").get).value must equal ("Tue, 29 Dec 2009 12:12:12 GMT")
   }
 
   "Date:  Should return none for badly formatted date" in {
-    HttpDateTimes.parseHttpDateTimes("Mon, 01-Jan-2001 00:00:00 UTC fooo baaaaar") mustEqual None
+    HttpDateTimes.parseHttpDateTimes("Mon, 01-Jan-2001 00:00:00 UTC fooo baaaaar") must equal (None)
   }
 
 }
