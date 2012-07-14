6ccdd193808110065b3ea8897e8c5c9184eac4dd
cheeseng <cheeseng@amaseng.com>
2012-07-14 19:42:32 +0800

    Rewritten HttpClientXLightWebSpec using ScalaTest.

--- core/src/test/scala/blueeyes/core/service/engines/HttpClientXLightWebSpec.scala
+++ core/src/test/scala/blueeyes/core/service/engines/HttpClientXLightWebSpec.scala
@@ -1,32 +1,33 @@
 package blueeyes.core.service.engines
 
 import blueeyes.core.http._
-import blueeyes.core.http.test._
+import blueeyes.core.http.test.HttpRequestCheckers
 import blueeyes.core.data._
 import blueeyes.core.service.HttpRequestHandlerCombinators
 import blueeyes.core.http.HttpHeaders._
 import blueeyes.core.http.MimeTypes._
 import blueeyes.core.http.HttpStatusCodes._
 import netty.NettyEngine
-import org.specs2.mutable.Specification
-import org.specs2.time.TimeConversions._
+import org.scalatest.WordSpec
+import org.scalatest.matchers.MustMatchers
+import org.scalatest.BeforeAndAfterAll
 
 import akka.dispatch.Future
 import akka.dispatch.Promise
 import akka.dispatch.Await
+import akka.util.Duration
 import blueeyes.bkka.AkkaDefaults
 
 import collection.mutable.ArrayBuilder.ofByte
 import blueeyes.json.JsonAST._
 import blueeyes.json.Printer._
-import org.specs2.specification.{Step, Fragments}
 
 import org.streum.configrity.Configuration
 import org.streum.configrity.io.BlockFormat
 
-class HttpClientXLightWebSpec extends Specification with BijectionsChunkString with ContentReader with HttpRequestHandlerCombinators 
-with AkkaDefaults with HttpRequestMatchers {
-  val duration = 250.milliseconds
+class HttpClientXLightWebSpec extends WordSpec with MustMatchers with BeforeAndAfterAll with HttpRequestCheckers with BijectionsChunkString with ContentReader with HttpRequestHandlerCombinators 
+with AkkaDefaults {
+  val duration = Duration(250, "milliseconds")
   val retries = 30
 
   private val httpClient = new HttpClientXLightWeb
@@ -40,8 +41,7 @@ with AkkaDefaults with HttpRequestMatchers {
   private var server: Option[NettyEngine] = None
   private var uri = ""
 
-  override def is = args(sequential = true) ^ super.is
-  override def map(fs: =>Fragments) = Step {
+  override def beforeAll() {
     var result: Option[EchoServer] = None 
     do {
       result = try {
@@ -64,7 +64,9 @@ with AkkaDefaults with HttpRequestMatchers {
     server = result 
 
     uri = "http://localhost:%s/echo".format(port)
-  } ^ fs ^ Step {
+  }
+
+  override def afterAll() {
     server.foreach{ _.stop }
   }
 
@@ -72,157 +74,133 @@ with AkkaDefaults with HttpRequestMatchers {
   "HttpClientXLightWeb" should {
     "Support GET to invalid server should return http error" in {
       val result = httpClient.get[String]("http://127.0.0.1:666/foo").failed
-      result must whenDelivered {
-        haveClass[HttpException]
-      }
+      result.futureValue.getClass must be (classOf[HttpException])
     }
 
     "Support GET to invalid URI/404 should cancel Future" in {
-      httpClient.get(uri + "/bogus").failed must whenDelivered {
-        beLike { case HttpException(NotFound, _) => ok }
+      httpClient.get(uri + "/bogus").failed.futureValue match {
+        case HttpException(NotFound, _) => // Ok
+        case other => fail("Expected HttpException(NotFound, _), but got: " + other)
       }
     }
 
     "Support GET requests with status OK" in {
-      httpClient.parameters('param1 -> "a").get(uri) must succeedWithContent {
-	(_ : String) => ok
+      httpClient.parameters('param1 -> "a").get(uri).futureValue.content.get match {
+        case _ : String => // Ok
+        case other => fail("Expected _ : String, but got " + other)
       }
     }
 
     "Support GET requests with status Not Found" in {
-      httpClient.get(uri + "/bogus").failed must whenDelivered {
-        beLike { case HttpException(NotFound, _) => ok }
+      httpClient.get(uri + "/bogus").failed.futureValue match {
+        case HttpException(NotFound, _) => // Ok
+        case other => fail("Expected HttpException(NotFound, _), but got: " + other)
       }
     }
 
     "Support GET requests with query params" in {
-      httpClient.get(uri + "?param1=a&param2=b") must succeedWithContent {
-        be_==/("param1=a&param2=b")
-      }
+      httpClient.get(uri + "?param1=a&param2=b").futureValue.content.get.trim must equal ("param1=a&param2=b".trim)
     }
 
     "Support GET requests with request params" in {
-      httpClient.parameters('param1 -> "a", 'param2 -> "b").get(uri) must succeedWithContent {
-        be_==/("param1=a&param2=b")
-      }
+      httpClient.parameters('param1 -> "a", 'param2 -> "b").get(uri).futureValue.content.get.trim must equal ("param1=a&param2=b".trim) 
     }
 
     "Support POST requests with query params" in {
-      httpClient.post(uri + "?param1=a&param2=b")("") must succeedWithContent {
-        be_==/("param1=a&param2=b")
-      }
+      httpClient.post(uri + "?param1=a&param2=b")("").futureValue.content.get.trim must equal ("param1=a&param2=b".trim)
     }
 
     "Support POST requests with request params" in {
-      httpClient.parameters('param1 -> "a", 'param2 -> "b").post(uri)("") must succeedWithContent {
-        be_==/("param1=a&param2=b")
-      }
+      httpClient.parameters('param1 -> "a", 'param2 -> "b").post(uri)("").futureValue.content.get.trim must equal ("param1=a&param2=b".trim)
     }
 
     "Support POST requests with body" in {
       val expected = "Hello, world"
-      httpClient.post(uri)(expected) must succeedWithContent (be_==(expected))
+      httpClient.post(uri)(expected).futureValue.content.get.trim must equal (expected.trim)
     }
 
     "Support POST requests with body and request params" in {
       val expected = "Hello, world"
-      httpClient.parameters('param1 -> "a", 'param2 -> "b").post(uri)(expected) must succeedWithContent {
-        be_==/("param1=a&param2=b" + expected)
-      }
+      httpClient.parameters('param1 -> "a", 'param2 -> "b").post(uri)(expected).futureValue.content.get.trim must equal (("param1=a&param2=b" + expected).trim)
     }
 
     "Support PUT requests with body" in {
       val expected = "Hello, world"
-      httpClient.header(`Content-Length`(100)).put(uri)(expected) must succeedWithContent {
-        be_==/(expected)
-      }
+      httpClient.header(`Content-Length`(100)).put(uri)(expected).futureValue.content.get.trim must equal (expected.trim)
     }
 
     "Support GET requests with header" in {
-      httpClient.header("Fooblahblah" -> "washere").header("param2" -> "1").get(uri + "?headers=true") must succeedWithContent {
-        contain("Fooblahblah: washere") and contain("param2: 1")
-      }
+      val content = httpClient.header("Fooblahblah" -> "washere").header("param2" -> "1").get(uri + "?headers=true").futureValue.content.get
+      content.indexOf("Fooblahblah: washere") must be >= 0
+      content.indexOf("param2: 1") must be >= 0
     }
 
     "Support POST requests with Content-Type: text/html & Content-Length: 100" in {
       val expected = "<html></html>"
-      httpClient.headers(`Content-Type`(text/html) :: `Content-Length`(100) :: Nil).post(uri)(expected) must whenDelivered {
-        beLike {
-          case HttpResponse(status, _, Some(content), _) =>
-            (status.code must_== HttpStatusCodes.OK) and
-            (content.trim must_== expected)
-        }
+      httpClient.headers(`Content-Type`(text/html) :: `Content-Length`(100) :: Nil).post(uri)(expected).futureValue match {
+        case HttpResponse(status, _, Some(content), _) =>
+          status.code must equal (HttpStatusCodes.OK)
+          content.trim must equal (expected)
+        case other => fail("Expected HttpResponse(status, _, Some(content), _), but got: " + other)
       }
     }
 
     "Support POST requests with large payload" in {
       val expected = Array.fill(2048*1000)(0).toList.mkString("")
-      httpClient.post(uri)(expected) must succeedWithContent {
-        be_==(expected)
-      }
+      httpClient.post(uri)(expected).futureValue.content.get must equal (expected)      
     }
     "Support POST requests with large payload with several chunks" in {
       val expected = Array.fill[Byte](2048*100)('0')
       val chunk   = Chunk(expected, Some(Future(Chunk(expected))))
-      httpClient.post[ByteChunk](uri)(chunk) must whenDelivered {
-        beLike {
-          case HttpResponse(status, _, Some(content), _) =>
-            (status.code must_== HttpStatusCodes.OK) and
-            (new String(content.data).length must_== new String(expected ++ expected).length)
-        }
+      httpClient.post[ByteChunk](uri)(chunk).futureValue match {
+        case HttpResponse(status, _, Some(content), _) =>
+          status.code must equal (HttpStatusCodes.OK)
+          new String(content.data).length must equal (new String(expected ++ expected).length)
+        case other => fail("Expected HttpResponse(status, _, Some(content), _), but got: " + other)
       }
     }
 
    "Support HEAD requests" in {
-      httpClient.head(uri) must whenDelivered {
-        beLike {
-          case HttpResponse(status, _, _, _) =>
-            (status.code must_== HttpStatusCodes.OK)
-        }
+      httpClient.head(uri).futureValue match {
+        case HttpResponse(status, _, _, _) =>
+          status.code must equal (HttpStatusCodes.OK)
+        case other => fail("Expected HttpResponse(status, _, _, _), but got: " + other)
       }
     }
 
    "Support response headers" in {
-      httpClient.get(uri) must whenDelivered {
-        beLike {
-          case HttpResponse(status, headers, _, _) =>
-            (status.code must_== HttpStatusCodes.OK) and
-            (headers.raw must haveKey("kludgy"))
-        }
+      httpClient.get(uri).futureValue match {
+        case HttpResponse(status, headers, _, _) =>
+          status.code must equal (HttpStatusCodes.OK)
+          headers.raw must contain key ("kludgy")
       }
     }
 
     "Support GET requests of 1000 requests" in {
       val total = 1000
-      val duration = 1000.milliseconds
+      val duration = Duration(1000, "milliseconds")
       val futures = List.fill(total) {
         httpClient.get(uri + "?test=true")
       }
 
-      Future.sequence(futures) must whenDelivered {
-        beLike {
-          case responses =>
-          (responses.size must_== total) and
-          forall(responses) {
-            response => response.status.code must_== HttpStatusCodes.OK
+      Future.sequence(futures).futureValue match {
+        case responses =>
+          responses.size must equal (total)
+          responses.foreach {
+            response => response.status.code must equal (HttpStatusCodes.OK)
           }
-        }
       }
     }
 
-    "Support GET requests with query params" in {
-      httpClient.get[String](uri + "?param1=a&param2=b") must succeedWithContent {
-        be_==/("param1=a&param2=b")
-      }
+    "Support typed GET requests with query params" in {
+      httpClient.get[String](uri + "?param1=a&param2=b").futureValue.content.get.trim must equal ("param1=a&param2=b")
     }
 
     "Support POST requests with body with Array[Byte]" in {
       import BijectionsChunkByteArray._
       import BijectionsByteArray._
       val expected = "Hello, world"
-      httpClient.post(uri)(StringToByteArray(expected)) must succeedWithContent {
-        (content: Array[Byte]) => ByteArrayToString(content) must_== expected
-      }
+      ByteArrayToString(httpClient.post(uri)(StringToByteArray(expected)).futureValue.content.get) must equal (expected)
     }
 
     "Support POST requests with body several chunks and transcoding" in {
@@ -233,20 +211,17 @@ with AkkaDefaults with HttpRequestMatchers {
       val chunks: Chunk[String] = Chunk("foo", Some(Future[Chunk[String]](Chunk("bar"))))
 
       val response = Await.result(httpClient.post(uri)(chunks)(bijection), duration)
-      response.status.code must be(HttpStatusCodes.OK)
-      readContent(response.content.get).map(_.mkString("")) must whenDelivered {
-        be_==("foobar")
-      }
+      response.status.code must be (HttpStatusCodes.OK)
+      readContent(response.content.get).map(_.mkString("")).futureValue must equal ("foobar")
     }
 
     "Support POST requests with encoded URL should be preserved" in {
       val content = "Hello, world"
-      httpClient.post(uri + "?headers=true&plckForumId=Cat:Wedding%20BoardsForum:238")(content) must whenDelivered {
-        beLike {
-          case HttpResponse(status, _, Some(content), _) =>
-            (status.code must_== HttpStatusCodes.OK) and
-            (content must contain("plckForumId=Cat:Wedding BoardsForum:238"))
-          }
+      httpClient.post(uri + "?headers=true&plckForumId=Cat:Wedding%20BoardsForum:238")(content).futureValue match {
+        case HttpResponse(status, _, Some(content), _) =>
+          status.code must equal (HttpStatusCodes.OK)
+          content.indexOf("plckForumId=Cat:Wedding BoardsForum:238") must be >= 0
+        case other => fail("Expected HttpResponse(status, _, Some(content), _), but got: " + other)
       }
     }
   }
