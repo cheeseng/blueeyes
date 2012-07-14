8541c2eb31b6329de8b70e4618a10a60637b7b50
cheeseng <cheeseng@amaseng.com>
2012-07-14 19:50:34 +0800

    Rewritten InetInterfacelookupSpec using ScalaTest.

--- core/src/test/scala/blueeyes/core/service/engines/InetInterfaceLookupSpec.scala
+++ core/src/test/scala/blueeyes/core/service/engines/InetInterfaceLookupSpec.scala
@@ -1,21 +1,22 @@
 package blueeyes.core.service.engines
 
-import org.specs2.mutable.Specification
+import org.scalatest.WordSpec
+import org.scalatest.matchers.MustMatchers
 import org.streum.configrity.Configuration 
 import org.streum.configrity.io.BlockFormat
 import java.net.{InetSocketAddress, InetAddress}
 
-class InetInterfaceLookupSpec extends Specification{
-  override def is = args(sequential = true) ^ super.is
+class InetInterfaceLookupSpec extends WordSpec with MustMatchers {
+
   "creates socket address when address is not configured" in{
     val config = Configuration.parse("", BlockFormat)
 
-    InetInterfaceLookup.socketAddres(config.detach("server"), 8080) mustEqual(new InetSocketAddress(8080))
+    InetInterfaceLookup.socketAddres(config.detach("server"), 8080) must equal (new InetSocketAddress(8080))
   }
   "creates host name when address is not configured" in{
     val config = Configuration.parse("", BlockFormat)
 
-    InetInterfaceLookup.host(config.detach("server")) mustEqual(InetAddress.getLocalHost().getHostName())
+    InetInterfaceLookup.host(config.detach("server")) must equal (InetAddress.getLocalHost().getHostName())
   }
   "creates socket address when address is configured" in{
     val rawConfig = """
@@ -26,7 +27,7 @@ class InetInterfaceLookupSpec extends Specification{
 
     val config = Configuration.parse(rawConfig, BlockFormat)
 
-    InetInterfaceLookup.socketAddres(config.detach("server"), 8080) mustEqual(new InetSocketAddress("192.168.10.10", 8080))
+    InetInterfaceLookup.socketAddres(config.detach("server"), 8080) must equal (new InetSocketAddress("192.168.10.10", 8080))
   }
   "creates host name when address is configured" in{
     val rawConfig = """
@@ -36,6 +37,6 @@ class InetInterfaceLookupSpec extends Specification{
     """
     val config = Configuration.parse(rawConfig, BlockFormat)
 
-    InetInterfaceLookup.host(config.detach("server")) mustEqual("192.168.10.10")
+    InetInterfaceLookup.host(config.detach("server")) must equal ("192.168.10.10")
   }
 }
