71006f933937383a9f4564be8dc1b6a821dcd788
cheeseng <cheeseng@amaseng.com>
2012-07-17 20:41:20 +0800

    Rewritten FullURIHttpRequestDecoderSpec using ScalaTest.

--- core/src/test/scala/blueeyes/core/service/engines/netty/FullURIHttpRequestDecoderSpec.scala
+++ core/src/test/scala/blueeyes/core/service/engines/netty/FullURIHttpRequestDecoderSpec.scala
@@ -1,27 +1,28 @@
 package blueeyes.core.service.engines.netty
 
-import org.specs2.mutable.Specification
+import org.scalatest.WordSpec
+import org.scalatest.matchers.MustMatchers
 import org.jboss.netty.handler.codec.http.{HttpMethod, DefaultHttpRequest, HttpVersion}
 
-class FullURIHttpRequestDecoderSpec extends Specification{
+class FullURIHttpRequestDecoderSpec extends WordSpec with MustMatchers {
   "creates full uri" in{
     val decoder = new FullURIHttpRequestDecoder("http", "google", 8080, 8192)
     val message = decoder.createMessage(Array("GET", "/foo", "HTTP/1.1")).asInstanceOf[DefaultHttpRequest]
 
-    message.getMethod           mustEqual(HttpMethod.GET)
-    message.getUri              mustEqual("http://google:8080/foo")
-    message.getProtocolVersion  mustEqual(HttpVersion.HTTP_1_1)
+    message.getMethod           must equal (HttpMethod.GET)
+    message.getUri              must equal ("http://google:8080/foo")
+    message.getProtocolVersion  must equal (HttpVersion.HTTP_1_1)
   }
   "creates full uri when first slash is missing" in{
     val decoder = new FullURIHttpRequestDecoder("http", "google", 8080, 8192)
     val message = decoder.createMessage(Array("GET", "foo", "HTTP/1.1")).asInstanceOf[DefaultHttpRequest]
 
-    message.getUri              mustEqual("http://google:8080/foo")
+    message.getUri              must equal ("http://google:8080/foo")
   }
   "creates full uri when original uri is full" in{
     val decoder = new FullURIHttpRequestDecoder("http", "google", 8080, 8192)
     val message = decoder.createMessage(Array("GET", "http://google:8080/foo", "HTTP/1.1")).asInstanceOf[DefaultHttpRequest]
 
-    message.getUri              mustEqual("http://google:8080/foo")
+    message.getUri              must equal ("http://google:8080/foo")
   }
 }
