1505270f348c343b77e24e4c94bec74553f7b3c3
cheeseng <cheeseng@amaseng.com>
2012-07-18 11:18:09 +0800

    Removed unused comments in BlueEyesServiceSpecificationSpec.

--- core/src/test/scala/blueeyes/core/service/test/BlueEyesServiceSpecificationSpec.scala
+++ core/src/test/scala/blueeyes/core/service/test/BlueEyesServiceSpecificationSpec.scala
@@ -1,6 +1,5 @@
 package blueeyes.core.service.test
 
-//import org.specs2.mutable.Specification
 import blueeyes.core.service._
 import akka.dispatch.Future
 import akka.dispatch.Promise
@@ -11,7 +10,6 @@ import blueeyes.core.http.MimeTypes._
 import blueeyes.core.http._
 import blueeyes.core.data.{ByteChunk, BijectionsChunkString}
 import TestService._
-//import org.specs2.time.TimeConversions._
 import blueeyes.concurrent.test.AkkaFutures
 import org.scalatest.time._
 import org.scalatest.concurrent.Eventually
