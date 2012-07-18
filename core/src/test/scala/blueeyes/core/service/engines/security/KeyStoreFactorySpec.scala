d1487cc62f78fe69c65a0b8c4af8414deeed4b3a
cheeseng <cheeseng@amaseng.com>
2012-07-18 09:55:03 +0800

    Rewritten KeyStoreFactorySpec using ScalaTest.

--- core/src/test/scala/blueeyes/core/service/engines/security/KeyStoreFactorySpec.scala
+++ core/src/test/scala/blueeyes/core/service/engines/security/KeyStoreFactorySpec.scala
@@ -1,13 +1,13 @@
 package blueeyes.core.service.engines.security
 
-import org.specs2.mutable.Specification
+import org.scalatest._
 
-class KeyStoreFactorySpec extends Specification with CertificateData{
+class KeyStoreFactorySpec extends WordSpec with MustMatchers with CertificateData{
   "KeyStoreFactory creates KeyStore" in{
     val data     = CertificateDecoder(encodedPrivateKey, encodedCertificate)
     val keyStore = KeyStoreFactory(data._1, data._2, "foo", "password")
 
-    keyStore.getCertificate("foo") must not be (null)
-    keyStore.getKey("foo", "password".toCharArray()) must not be (null)
+    keyStore.getCertificate("foo") must not be null
+    keyStore.getKey("foo", "password".toCharArray()) must not be null
   }
 }
