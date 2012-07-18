ce5d6fd0f9f7a2518b7f0d71e80ccd196ae3af31
cheeseng <cheeseng@amaseng.com>
2012-07-18 10:54:45 +0800

    Removed unused comments in HttpServerServletSpec.

--- core/src/test/scala/blueeyes/core/service/engines/servlet/HttpServerServletSpec.scala
+++ core/src/test/scala/blueeyes/core/service/engines/servlet/HttpServerServletSpec.scala
@@ -10,8 +10,6 @@ import blueeyes.core.http._
 import blueeyes.core.data.{ByteChunk, BijectionsByteArray, BijectionsChunkString}
 import blueeyes.core.http.HttpStatusCodes._
 
-//import org.specs2.specification.{Step, Fragments}
-//import org.specs2.time.TimeConversions._
 import org.scalatest.BeforeAndAfterAll
 import blueeyes.concurrent.test.AkkaFutures
 import akka.dispatch.{Await, Promise}
@@ -20,8 +18,6 @@ import java.util.concurrent.TimeUnit
 
 class HttpServerServletSpec extends WordSpec with MustMatchers with BeforeAndAfterAll with BijectionsByteArray with BijectionsChunkString with blueeyes.bkka.AkkaDefaults with AkkaFutures {
   val duration = Duration(10000, "millis")
-  //val retries = 50
-  //implicit val futureTimeouts: FutureTimeouts = FutureTimeouts(10, Duration(1000l, TimeUnit.MILLISECONDS))
 
   private var port= 8585
   private var server: Option[JettyServer] = None
@@ -48,27 +44,6 @@ class HttpServerServletSpec extends WordSpec with MustMatchers with BeforeAndAft
     server.foreach(_.stop)
   }
 
-  /*override def is = args(sequential = true) ^ super.is
-  override def map(fs: =>Fragments) = Step {
-    while(!server.isDefined){
-      try{
-        val server = new JettyServer(new ServletTestEngineService())
-        server.start(port)
-
-        this.server = Some(server)
-      }
-      catch {
-        case e: Throwable => {
-          e.printStackTrace()
-          port = port + 1;
-        }
-      }
-    }
-  } ^ fs ^ Step {
-    TestEngineServiceContext.dataFile.delete
-    server.foreach(_.stop)
-  }*/
-
   "HttpServer" should {
     "return empty response"in{
       client.post("/empty/response")("").futureValue match {
@@ -77,13 +52,6 @@ class HttpServerServletSpec extends WordSpec with MustMatchers with BeforeAndAft
           content must be (None)
         case other => fail("Expected HttpResponse(status, _, content, _), but got: " + other)
       }
-      /*client.post("/empty/response")("") must whenDelivered {
-        beLike {
-          case HttpResponse(status, _, content, _) =>
-            (status.code must be (OK)) and
-              (content must beNone)
-        }
-      }*/
     }
 
     "write file"in{
@@ -95,14 +63,6 @@ class HttpServerServletSpec extends WordSpec with MustMatchers with BeforeAndAft
           TestEngineServiceContext.dataFile.length must equal ("foo".length)
         case other => fail("Expected HttpResponse(status, _, content, _), but got: " + other)
       }
-      /*client.post("/file/write")("foo") must whenDelivered {
-        beLike {
-          case HttpResponse(status, _, content, _) =>
-            (status.code must be (OK)) and
-              (TestEngineServiceContext.dataFile.exists must be_==(true)) and
-              (TestEngineServiceContext.dataFile.length mustEqual("foo".length))
-        }
-      }*/
     }
 
     "read file"in{
@@ -115,13 +75,6 @@ class HttpServerServletSpec extends WordSpec with MustMatchers with BeforeAndAft
           content must be (Some("foo"))
         case other => fail("Expected HttpResponse(status, _, content, _), but got: " + other)
       }
-      /*client.get("/file/read") must whenDelivered {
-        beLike {
-          case HttpResponse(status, _, content, _) =>
-            (status.code must be (OK)) and
-              (content must beSome("foo"))
-        }
-      }*/
     }
 
     "return html by correct URI" in{
@@ -131,18 +84,10 @@ class HttpServerServletSpec extends WordSpec with MustMatchers with BeforeAndAft
           content must be (Some(TestEngineServiceContext.context))
         case other => fail("Expected HttpResponse(status, _, content, _), but got: " + other)
       }
-      /*client.get("/bar/foo/adCode.html") must whenDelivered {
-        beLike {
-          case HttpResponse(status, _, content, _) =>
-            (status.code must be (OK)) and
-              (content must beSome(TestEngineServiceContext.context))
-        }
-      }*/
     }
 
     "return NotFound when accessing a nonexistent URI" in{
       val response = Await.result(client.get("/foo/foo/adCode.html").failed, duration)
-      //response must beLike { case HttpException(failure, _) => failure must_== NotFound }
       response match {
         case HttpException(failure, _) => failure must equal (NotFound)
         case other => fail("Expected HttpException(failure, _), but got: " + other)
@@ -150,7 +95,6 @@ class HttpServerServletSpec extends WordSpec with MustMatchers with BeforeAndAft
     }
     "return InternalServerError when handling request crashes" in{
       val response = Await.result(client.get("/error").failed, duration)
-      //response must beLike { case HttpException(failure, _) => failure must_== InternalServerError }
       response match {
         case HttpException(failure, _) => failure must equal (InternalServerError)
         case other => fail("Expected HttpException(failure, _), but got: " + other)
@@ -158,7 +102,6 @@ class HttpServerServletSpec extends WordSpec with MustMatchers with BeforeAndAft
     }
     "return Http error when handling request throws HttpException" in {
       val response = Await.result(client.get("/http/error").failed, duration)
-      //response must beLike { case HttpException(failure, _) => failure must_== BadRequest }
       response match {
         case HttpException(failure, _) => failure must equal (BadRequest)
         case other => fail("Expected HttpException(failure, _), but got: " + other)
@@ -167,13 +110,6 @@ class HttpServerServletSpec extends WordSpec with MustMatchers with BeforeAndAft
 
     "return html by correct URI with parameters" in{
       client.parameters('bar -> "zar").get("/foo")
-      /*client.parameters('bar -> "zar").get("/foo") must whenDelivered {
-        beLike {
-          case HttpResponse(status, _, content, _) =>
-            (status.code must be (OK)) and
-              (content must beSome(TestEngineServiceContext.context))
-        }
-      }*/
     }
     "return huge content"in{
       client.get[ByteChunk]("/huge").futureValue match {
@@ -182,13 +118,6 @@ class HttpServerServletSpec extends WordSpec with MustMatchers with BeforeAndAft
           content.map(v => readContent(v)) must be (Some(TestEngineServiceContext.hugeContext.map(v => new String(v).mkString("")).mkString("")))
         case other => fail("Expected HttpResponse(status, _, content, _), but got: " + other)
       }
-      /*client.get[ByteChunk]("/huge") must whenDelivered {
-        beLike {
-          case HttpResponse(status, _, content, _) =>
-            (status.code must be (OK)) and
-              (content.map(v => readContent(v)) must beSome(TestEngineServiceContext.hugeContext.map(v => new String(v).mkString("")).mkString("")))
-        }
-      }*/
     }
     "return huge delayed content"in{
       val content = client.get[ByteChunk]("/huge/delayed")
@@ -199,13 +128,6 @@ class HttpServerServletSpec extends WordSpec with MustMatchers with BeforeAndAft
         case other => 
           fail("Expected HttpResponse(status, _, content, _), but got: " + other)
       }
-      /*content must whenDelivered {
-        beLike {
-          case HttpResponse(status, _, content, _) =>
-            (status.code must be (OK)) and
-              (content.map(v => readContent(v)) must beSome(TestEngineServiceContext.hugeContext.map(v => new String(v).mkString("")).mkString("")))
-        }
-      }*/
     }
   }
 
