4439cf8e2326977f1d1e52f6e937be13dd34c070
cheeseng <cheeseng@amaseng.com>
2012-07-18 12:22:10 +0800

    Rewritten IntervalParserSpec using ScalaTest.

--- core/src/test/scala/blueeyes/health/metrics/IntervalParserSpec.scala
+++ core/src/test/scala/blueeyes/health/metrics/IntervalParserSpec.scala
@@ -1,24 +1,24 @@
 package blueeyes.health.metrics
 
-import org.specs2.mutable.Specification
+import org.scalatest._
 import java.util.concurrent.TimeUnit
 
-class IntervalParserSpec extends Specification{
+class IntervalParserSpec extends WordSpec with MustMatchers{
   "IntervalParser" should{
     "parse 'seconds' interval" in{
-      IntervalParser.parse("30s x 10") mustEqual (interval(IntervalLength(30, TimeUnit.SECONDS), 10))
-      IntervalParser.parse("3 s x 10") mustEqual (interval(IntervalLength(3, TimeUnit.SECONDS), 10))
+      IntervalParser.parse("30s x 10") must equal (interval(IntervalLength(30, TimeUnit.SECONDS), 10))
+      IntervalParser.parse("3 s x 10") must equal (interval(IntervalLength(3, TimeUnit.SECONDS), 10))
     }
     "parse 'minutes' interval" in{
-      IntervalParser.parse("30min x 10") mustEqual (interval(IntervalLength(30, TimeUnit.MINUTES), 10))
-      IntervalParser.parse("3 min x 10") mustEqual (interval(IntervalLength(3, TimeUnit.MINUTES), 10))
+      IntervalParser.parse("30min x 10") must equal (interval(IntervalLength(30, TimeUnit.MINUTES), 10))
+      IntervalParser.parse("3 min x 10") must equal (interval(IntervalLength(3, TimeUnit.MINUTES), 10))
     }
     "parse 'hours' interval" in{
-      IntervalParser.parse("3h x 10") mustEqual (interval(IntervalLength(3, TimeUnit.HOURS), 10))
-      IntervalParser.parse("30 h x 10") mustEqual (interval(IntervalLength(30, TimeUnit.HOURS), 10))
+      IntervalParser.parse("3h x 10") must equal (interval(IntervalLength(3, TimeUnit.HOURS), 10))
+      IntervalParser.parse("30 h x 10") must equal (interval(IntervalLength(30, TimeUnit.HOURS), 10))
     }
     "parse 'eternity' interval" in{
-      IntervalParser.parse("eternity") mustEqual (eternity)
+      IntervalParser.parse("eternity") must equal (eternity)
     }
   }
 }
