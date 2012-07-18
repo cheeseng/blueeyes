efe705fdcbdc8249491a77dda3eda1f1faf106d8
cheeseng <cheeseng@amaseng.com>
2012-07-18 09:48:54 +0800

    Rewritten CertificateGeneratorSpec using ScalaTest.

--- core/src/test/scala/blueeyes/core/service/engines/security/CertificateGeneratorSpec.scala
+++ core/src/test/scala/blueeyes/core/service/engines/security/CertificateGeneratorSpec.scala
@@ -1,11 +1,11 @@
 package blueeyes.core.service.engines.security
 
-import org.specs2.mutable.Specification
+import org.scalatest._
 
-class CertificateGeneratorSpec extends Specification{
+class CertificateGeneratorSpec extends WordSpec with MustMatchers{
   "Creates key and certificate" in{
     val keyAndCertificate     = CertificateGenerator("RSA", "test", "CN=foo.example.com,L=Melbourne,ST=Victoria,C=AU", 36500, "password")
 
-    keyAndCertificate must not be (null)
+    keyAndCertificate must not be null
   }
 }
