e731255a90c3a715df16adcf9fd7510e6bb30a07
cheeseng <cheeseng@amaseng.com>
2012-07-19 12:43:04 +0800

    Rewritten CacheSpec using ScalaTest.

--- core/src/test/scala/blueeyes/persistence/cache/CacheSpec.scala
+++ core/src/test/scala/blueeyes/persistence/cache/CacheSpec.scala
@@ -1,9 +1,15 @@
 package blueeyes.persistence.cache
 
-import org.specs2.mutable.Specification
+import org.scalatest._
+import org.scalatest.time._
 import java.util.concurrent.TimeUnit.{MILLISECONDS}
+import org.scalatest.concurrent.Eventually
 
-class CacheSpec extends Specification{
+class CacheSpec extends WordSpec with MustMatchers with Eventually{
+
+  override implicit val patienceConfig =
+    PatienceConfig(timeout = scaled(Span(3, Seconds)), interval = scaled(Span(100, Millis)))
+
   "Cache.concurrent: creates new map" in {
     Cache.concurrent(settings()) must not be (null)
   }
@@ -11,7 +17,7 @@ class CacheSpec extends Specification{
     val map = Cache.concurrent(settings())
     map.put("foo", "bar")
 
-    map.get("foo") must beSome("bar")
+    map.get("foo") must be (Some("bar"))
   }
 
   "Cache.concurrent.put: evict eldest entry" in {
@@ -20,34 +26,34 @@ class CacheSpec extends Specification{
     map.put("foo", "bar")
     map.put("baz", "foo")
 
-    evicted        must be_==(true)
-    map.get("foo") must beNone
-    map.get("baz") must beSome("foo")
+    evicted        must equal (true)
+    map.get("foo") must be (None)
+    map.get("baz") must be (Some("foo"))
   }
 
   "Cache.concurrent.put: evicts when idle time is expired" in{
     val map = Cache.concurrent(settings(Some(50)))
     map.put("baz", "bar")
-    map.contains("baz") must eventually (be_==(false))
+    eventually { map.contains("baz") must equal (false) }
   }
   "Cache.concurrent.put: evicts when live time is expired" in{
     val map = Cache.concurrent(settings(None, Some(200)))
     map.put("baz", "bar")
-    map.contains("baz")  must eventually (be_==(false))
+    eventually { map.contains("baz")  must equal (false) }
   }
   "Cache.concurrent: evict is called when entry is expired" in{
     var expired = false
     val map = Cache.concurrent(settings(None, Some(200), {(key: String, value: String) => expired = key == "foo" && value == "bar"}))
     map.put("foo", "bar")
 
-    map.contains("foo") must eventually (be_==(false))
-    expired must be_==(true)
+    eventually { map.contains("foo") must equal (false) }
+    expired must equal (true)
   }
-  "Cache.concurrent: adds new Entry" in {
+  "Cache.concurrent: adds new Entry 2" in {  // This is identical to the first test, should be removed.
     val map = Cache.concurrent(settings())
     map.put("foo", "bar")
 
-    map.get("foo") must beSome("bar")
+    map.get("foo") must be (Some("bar"))
   }
 
   private def settings(timeToIdle: Option[Long] = None, timeToLive: Option[Long] = None,
