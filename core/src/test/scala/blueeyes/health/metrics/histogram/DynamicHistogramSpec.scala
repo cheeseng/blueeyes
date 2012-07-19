62cec8cafecce72ed547743d082ecbcf05b63161
cheeseng <cheeseng@amaseng.com>
2012-07-19 11:15:19 +0800

    Rewritten DynamicHistogramSpec using ScalaTest.

--- core/src/test/scala/blueeyes/health/metrics/histogram/DynamicHistogramSpec.scala
+++ core/src/test/scala/blueeyes/health/metrics/histogram/DynamicHistogramSpec.scala
@@ -1,25 +1,25 @@
 package blueeyes.health.metrics
 package histogram
 
-import org.specs2.mutable.Specification
+import org.scalatest._
 import ValueStrategy._
 import java.util.concurrent.TimeUnit
 
-class DynamicHistogramSpec extends Specification with TimedStatFixtures {
+class DynamicHistogramSpec extends WordSpec with MustMatchers with TimedStatFixtures {
   "DynamicHistogram" should{
     "build Histogram" in{
       val histogram = DynamicHistogram.empty[Long](3, 3, TimeUnit.SECONDS)
 
       val result    = set(histogram, 15500, 16600, 18100, 21100, 21201, 21310)
 
-      result.histogram mustEqual(Map(15 -> 2, 18 -> 1, 21 -> 3))
+      result.histogram must equal (Map(15 -> 2, 18 -> 1, 21 -> 3))
     }
 
     "fill last missing buckets" in{
       val histogram = DynamicHistogram.empty[Long](3, 3, TimeUnit.SECONDS)
 
       val result    = set(histogram, 18100, 21100, 21201, 21310)
-      result.histogram mustEqual(Map(15 -> 0, 18 -> 1, 21 -> 3))
+      result.histogram must equal (Map(15 -> 0, 18 -> 1, 21 -> 3))
     }
 
     "fill first missing buckets" in{
@@ -27,7 +27,7 @@ class DynamicHistogramSpec extends Specification with TimedStatFixtures {
 
       val result    = set(histogram, 15500, 16600, 18100)
       clock.setNow(21100)
-      result.histogram mustEqual(Map(15 -> 2, 18 -> 1, 21 -> 0))
+      result.histogram must equal (Map(15 -> 2, 18 -> 1, 21 -> 0))
     }
 
     "remove expired buckets" in{
@@ -35,7 +35,7 @@ class DynamicHistogramSpec extends Specification with TimedStatFixtures {
 
       val result    = set(histogram, 12100, 15500, 16600, 18100, 21100, 21201, 21310)
 
-      result.histogram mustEqual(Map(15 -> 2, 18 -> 1, 21 -> 3))
+      result.histogram must equal (Map(15 -> 2, 18 -> 1, 21 -> 3))
     }
   }
 
