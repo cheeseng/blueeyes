7e8e12dc05cbb1114e0ee981d106b14394ee3836
cheeseng <cheeseng@amaseng.com>
2012-07-19 16:22:51 +0800

    Rewritten RequestLoggerSpec using ScalaTest.

--- core/src/test/scala/blueeyes/util/logging/RequestLoggerSpec.scala
+++ core/src/test/scala/blueeyes/util/logging/RequestLoggerSpec.scala
@@ -1,19 +1,21 @@
 package blueeyes.util.logging
 
-import org.specs2.mutable.Specification
+import org.scalatest._
 import blueeyes.parsers.W3ExtendedLogAST._
 import RollPolicies._
 import java.io.File
 import scala.io.Source
 import blueeyes.core.service.HttpRequestLoggerW3CFormatter
-import org.specs2.specification.AfterExample
 import akka.util.Timeout
+import org.scalatest.concurrent.Eventually
+import org.scalatest.time._
 
-class RequestLoggerSpec extends Specification with AfterExample{
+class RequestLoggerSpec extends WordSpec with MustMatchers with Eventually with BeforeAndAfter {
   private val directives = FieldsDirective(List(DateIdentifier, TimeIdentifier))
   private val formatter  = new HttpRequestLoggerW3CFormatter()
 
-  override def is = args(sequential = true) ^ super.is
+  override implicit val patienceConfig =
+    PatienceConfig(timeout = scaled(Span(3, Seconds)), interval = scaled(Span(100, Millis)))
 
   private var w3Logger: RequestLogger = _
   "W3ExtendedLogger" should {
@@ -22,16 +24,16 @@ class RequestLoggerSpec extends Specification with AfterExample{
     "creates log file" in {
       w3Logger = RequestLogger.get(System.getProperty("java.io.tmpdir") + File.separator + "w3.log", Never, header _, 1)
 
-      new File(w3Logger.fileName.get).exists must be_==(true)
+      new File(w3Logger.fileName.get).exists must equal (true)
     }
     "init log file" in {
       w3Logger = RequestLogger.get(System.getProperty("java.io.tmpdir") + File.separator + "w3_1.log", Never, header _, 1)
 
       val content = getContents(new File(w3Logger.fileName.get))
 
-      content.indexOf("#Version: 1.0")      must_!= (-1)
-      content.indexOf("#Date: ")            must_!= (-1)
-      content.indexOf(directives.toString)  must_!= (-1)
+      content.indexOf("#Version: 1.0")      must not equal (-1)
+      content.indexOf("#Date: ")            must not equal (-1)
+      content.indexOf(directives.toString)  must not equal (-1)
     }
 
     "flush entries while closing" in{
@@ -40,13 +42,13 @@ class RequestLoggerSpec extends Specification with AfterExample{
       w3Logger("foo")
       w3Logger("bar")
 
-      val future = w3Logger.close(Timeout(5000))
-      future.value must eventually (beSome)
+      val future = w3Logger.close(akka.util.Timeout(5000))
+      eventually { future.value.isDefined must be (true) }
 
       val content = getContents(new File(w3Logger.fileName.get))
 
-      content.indexOf("foo") must_!= (-1)
-      content.indexOf("bar") must_!= (-1)
+      content.indexOf("foo") must not equal (-1)
+      content.indexOf("bar") must not equal (-1)
     }
 
     "write log entries" in {
@@ -58,19 +60,19 @@ class RequestLoggerSpec extends Specification with AfterExample{
 
       val file = new File(w3Logger.fileName.get)
 
-      getContents(file).indexOf("foo") must eventually(not (be_== (-1)))
-      getContents(file).indexOf("bar") must eventually(not (be_== (-1)))
+      eventually { getContents(file).indexOf("foo") must not equal (-1) }
+      eventually { getContents(file).indexOf("bar") must not equal (-1) }
     }
   }
 
   def cleanUp(){
-    val future = w3Logger.close(Timeout(5000))
-    future.value must eventually (beSome)
+    val future = w3Logger.close(akka.util.Timeout(5000))
+    eventually { future.value.isDefined must be (true) }
 
     new File(w3Logger.fileName.get).delete
   }
 
-  protected def after = cleanUp()
+  after { cleanUp() }
 
   private def getContents(file: File) = Source.fromFile(file, "UTF-8").getLines.mkString("\n")
 }
