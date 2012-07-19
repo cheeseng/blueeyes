c19b9942b8ab05817c573830d26c903eebdee715
cheeseng <cheeseng@amaseng.com>
2012-07-19 16:23:14 +0800

    Rewritten RollSpec using ScalaTest.

--- core/src/test/scala/blueeyes/util/logging/RollSpec.scala
+++ core/src/test/scala/blueeyes/util/logging/RollSpec.scala
@@ -1,28 +1,28 @@
 package blueeyes.util.logging
 
 import RollPolicies._
-import org.specs2.mutable.Specification
+import org.scalatest._
 import java.util.{Calendar, GregorianCalendar}
 
-class RollSpec extends Specification with Roll{
+class RollSpec extends WordSpec with MustMatchers with Roll{
   "Roll: calculates nextRollTime with Never policy" in {
     val next = calendar(1)
     next.add(Calendar.YEAR, 100)
 
-    nextRollTime(Never, 1) mustEqual(next.getTimeInMillis)
+    nextRollTime(Never, 1) must equal (next.getTimeInMillis)
   }
   "Roll: calculates nextRollTime with Hourly policy" in {
     val next = calendar(1)
     next.add(Calendar.HOUR_OF_DAY, 1)
 
-    nextRollTime(Hourly, 1) mustEqual(next.getTimeInMillis)
+    nextRollTime(Hourly, 1) must equal (next.getTimeInMillis)
   }
   "Roll: calculates nextRollTime with Daily policy" in {
     val next = calendar(1)
     next.set(Calendar.HOUR_OF_DAY, 0)
     next.add(Calendar.DAY_OF_MONTH, 1)
 
-    nextRollTime(Daily, 1) mustEqual(next.getTimeInMillis)
+    nextRollTime(Daily, 1) must equal (next.getTimeInMillis)
   }
   "Roll: calculates nextRollTime with Weekly policy" in {
     val next = calendar(1)
@@ -31,7 +31,7 @@ class RollSpec extends Specification with Roll{
       next.add(Calendar.DAY_OF_MONTH, 1)
     } while (next.get(Calendar.DAY_OF_WEEK) != 1)
 
-    nextRollTime(Weekly(1), 1) mustEqual(next.getTimeInMillis)
+    nextRollTime(Weekly(1), 1) must equal (next.getTimeInMillis)
   }
 
   private def calendar(now: Long) = {
