02eb28ab66818192b80b6c6e181cf204ac2a4557
cheeseng <cheeseng@amaseng.com>
2012-07-19 11:19:50 +0800

    Rewritten TimerHistogramSpec using ScalaTest.

--- core/src/test/scala/blueeyes/health/metrics/histogram/TimerHistogramSpec.scala
+++ core/src/test/scala/blueeyes/health/metrics/histogram/TimerHistogramSpec.scala
@@ -1,26 +1,26 @@
 package blueeyes.health.metrics.histogram
 
 import blueeyes.util.metrics.Duration._
-import org.specs2.mutable.Specification
+import org.scalatest._
 
 import ValueStrategy._
 import blueeyes.health.metrics.Timer
 
-class TimerHistogramSpec extends Specification{
+class TimerHistogramSpec extends WordSpec with MustMatchers {
   "TimerHistogram" should{
     "create Timer Histogram" in{
       val histogram = new StaticHistogram[Timer](new DynamicLengthBucketsStrategy()).histogram(Map(1.2 -> 2l, 1.5 -> 2l, 5.9 -> 1l, 12.1 -> 3l).toList.sortWith((e1, e2) => (e1._1 < e2._1)))
-      histogram.get(1).get.count mustEqual(2)
-      histogram.get(1).get.min mustEqual(2.nanoseconds)
+      histogram.get(1).get.count must equal (2)
+      histogram.get(1).get.min must equal (2.nanoseconds)
 
-      histogram.get(4).get.count mustEqual(1)
-      histogram.get(4).get.min mustEqual(1.nanoseconds)
+      histogram.get(4).get.count must equal (1)
+      histogram.get(4).get.min must equal (1.nanoseconds)
 
-      histogram.get(7).get.count mustEqual(0)
-      histogram.get(7).get.min mustEqual(0.nanoseconds)
+      histogram.get(7).get.count must equal (0)
+      histogram.get(7).get.min must equal (0.nanoseconds)
 
-      histogram.get(10).get.count mustEqual(1)
-      histogram.get(10).get.min mustEqual(3.nanoseconds)
+      histogram.get(10).get.count must equal (1)
+      histogram.get(10).get.min must equal (3.nanoseconds)
     }
   }
 }
