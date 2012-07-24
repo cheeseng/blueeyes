eddea19a63fa55590c9084dda9218af8fdc8c1a7
cheeseng <cheeseng@amaseng.com>
2012-07-24 18:16:56 +0800

    Rewritten MongoEnsureIndexQuerySpec using ScalaTest.

--- mongo/src/test/scala/blueeyes/persistence/mongo/MongoEnsureIndexQuerySpec.scala
+++ mongo/src/test/scala/blueeyes/persistence/mongo/MongoEnsureIndexQuerySpec.scala
@@ -1,14 +1,14 @@
 package blueeyes.persistence.mongo
 
-import org.specs2.mutable.Specification
+import org.scalatest._
 import MongoFilterOperators._
 import blueeyes.json.JPath
 import blueeyes.json.JPathImplicits._
 
-class MongoEnsureIndexQuerySpec extends Specification with MongoImplicits{
+class MongoEnsureIndexQuerySpec extends WordSpec with MustMatchers with MongoImplicits{
   private val query = ensureIndex("foo").on("bar").in("collection")
 
   "'geospatial' method sets geospatial index type" in {
-    query.geospatial("bar") mustEqual (MongoEnsureIndexQuery("collection", "foo", List(Tuple2(JPath("bar"), GeospatialIndex)), false))
+    query.geospatial("bar") must equal (MongoEnsureIndexQuery("collection", "foo", List(Tuple2(JPath("bar"), GeospatialIndex)), false))
   }
 }
