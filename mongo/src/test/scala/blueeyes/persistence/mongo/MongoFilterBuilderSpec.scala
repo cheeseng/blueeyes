c42ecf0943987299380d6dc446dec6683d4b4a5f
cheeseng <cheeseng@amaseng.com>
2012-07-24 18:18:35 +0800

    Rewritten MongoFilterBuilderSpec using ScalaTest.

--- mongo/src/test/scala/blueeyes/persistence/mongo/MongoFilterBuilderSpec.scala
+++ mongo/src/test/scala/blueeyes/persistence/mongo/MongoFilterBuilderSpec.scala
@@ -1,67 +1,67 @@
 package blueeyes.persistence.mongo
 
-import org.specs2.mutable.Specification
+import org.scalatest._
 import MongoFilterOperators._
 import blueeyes.json.JPath
 import blueeyes.json.JsonAST._
 import MongoFilterImplicits._
 
-class MongoFilterBuilderSpec extends Specification{
+class MongoFilterBuilderSpec extends WordSpec with MustMatchers {
 
   "builds $eq operation" in {
-    JPath("foo") === "bar" mustEqual (MongoFieldFilter("foo", $eq, MongoPrimitiveString("bar")))
+    JPath("foo") === "bar" must equal (MongoFieldFilter("foo", $eq, MongoPrimitiveString("bar")))
   }
   "builds $ne operation" in {
-    (JPath("foo") !== 1) mustEqual (MongoFieldFilter("foo", $ne, MongoPrimitiveInt(1)))
+    (JPath("foo") !== 1) must equal (MongoFieldFilter("foo", $ne, MongoPrimitiveInt(1)))
   }
   "builds $gt operation" in {
-    JPath("foo") > 1l mustEqual (MongoFieldFilter("foo", $gt, MongoPrimitiveLong(1l)))
+    JPath("foo") > 1l must equal (MongoFieldFilter("foo", $gt, MongoPrimitiveLong(1l)))
   }
   "builds $gte operation" in {
-    JPath("foo") >= 1.1 mustEqual (MongoFieldFilter("foo", $gte, MongoPrimitiveDouble(1.1)))
+    JPath("foo") >= 1.1 must equal (MongoFieldFilter("foo", $gte, MongoPrimitiveDouble(1.1)))
   }
   "builds $lte operation" in {
-    JPath("foo") <= 1.1 mustEqual (MongoFieldFilter("foo", $lte, MongoPrimitiveDouble(1.1)))
+    JPath("foo") <= 1.1 must equal (MongoFieldFilter("foo", $lte, MongoPrimitiveDouble(1.1)))
   }
   "builds $in operation" in {
-    JPath("foo").anyOf(MongoPrimitiveString("foo"), MongoPrimitiveString("bar")) mustEqual (MongoFieldFilter("foo", $in, MongoPrimitiveArray(MongoPrimitiveString("foo") :: MongoPrimitiveString("bar") :: Nil)))
+    JPath("foo").anyOf(MongoPrimitiveString("foo"), MongoPrimitiveString("bar")) must equal (MongoFieldFilter("foo", $in, MongoPrimitiveArray(MongoPrimitiveString("foo") :: MongoPrimitiveString("bar") :: Nil)))
   }
   "builds $all operation" in {
-    JPath("foo").contains(MongoPrimitiveString("foo"), MongoPrimitiveString("bar")) mustEqual (MongoFieldFilter("foo", $all, MongoPrimitiveArray(MongoPrimitiveString("foo") :: MongoPrimitiveString("bar") :: Nil)))
+    JPath("foo").contains(MongoPrimitiveString("foo"), MongoPrimitiveString("bar")) must equal (MongoFieldFilter("foo", $all, MongoPrimitiveArray(MongoPrimitiveString("foo") :: MongoPrimitiveString("bar") :: Nil)))
   }
 
   "builds $size operation" in {
-    JPath("foo").hasSize(1) mustEqual (MongoFieldFilter("foo", $size, MongoPrimitiveInt(1)))
+    JPath("foo").hasSize(1) must equal (MongoFieldFilter("foo", $size, MongoPrimitiveInt(1)))
   }
   "builds $exists operation" in {
-    MongoFilterBuilder(JPath("foo")).isDefined mustEqual (MongoFieldFilter("foo", $exists, MongoPrimitiveBoolean(true)))
+    MongoFilterBuilder(JPath("foo")).isDefined must equal (MongoFieldFilter("foo", $exists, MongoPrimitiveBoolean(true)))
   }
   "builds $hasType operation" in {
-    JPath("foo").hasType[JString] mustEqual (MongoFieldFilter("foo", $type, MongoPrimitiveInt(2)))
+    JPath("foo").hasType[JString] must equal (MongoFieldFilter("foo", $type, MongoPrimitiveInt(2)))
   }
   "builds $regex operation" in {
-    ("foo" regex "bar") mustEqual (MongoFieldFilter("foo", $regex, MongoPrimitiveJObject(JObject(List(JField("$regex", JString("bar")), JField("$options", JString("")))))))
+    ("foo" regex "bar") must equal (MongoFieldFilter("foo", $regex, MongoPrimitiveJObject(JObject(List(JField("$regex", JString("bar")), JField("$options", JString("")))))))
   }
   "builds $regex operation with options" in {
-    ("foo" regex ("bar", "i")) mustEqual (MongoFieldFilter("foo", $regex, MongoPrimitiveJObject(JObject(List(JField("$regex", JString("bar")), JField("$options", JString("i")))))))
+    ("foo" regex ("bar", "i")) must equal (MongoFieldFilter("foo", $regex, MongoPrimitiveJObject(JObject(List(JField("$regex", JString("bar")), JField("$options", JString("i")))))))
   }
   "builds $near" in {
-    ("foo" near (50, 60)) mustEqual (MongoFieldFilter("foo", $near, MongoPrimitiveJObject(JObject(List(JField("$near", JArray(List(JDouble(50.0), JDouble(60.0)))))))))
+    ("foo" near (50, 60)) must equal (MongoFieldFilter("foo", $near, MongoPrimitiveJObject(JObject(List(JField("$near", JArray(List(JDouble(50.0), JDouble(60.0)))))))))
   }
   "builds $within for box" in {
-    ("foo" within Box((10, 20), (30, 40))) mustEqual (MongoFieldFilter("foo", $within, MongoPrimitiveJObject(JObject(JField("$within", JObject(JField("$box", JArray(JArray(JDouble(10.0) :: JDouble(20.0) :: Nil) :: JArray(JDouble(30.0) :: JDouble(40.0) :: Nil) :: Nil)) :: Nil)) :: Nil))))
+    ("foo" within Box((10, 20), (30, 40))) must equal (MongoFieldFilter("foo", $within, MongoPrimitiveJObject(JObject(JField("$within", JObject(JField("$box", JArray(JArray(JDouble(10.0) :: JDouble(20.0) :: Nil) :: JArray(JDouble(30.0) :: JDouble(40.0) :: Nil) :: Nil)) :: Nil)) :: Nil))))
   }
   "builds $within for circe" in {
-    ("foo" within Circle((10, 20), 30)) mustEqual (MongoFieldFilter("foo", $within, MongoPrimitiveJObject(JObject(JField("$within", JObject(JField("$center",  JArray(JArray(JDouble(10.0) :: JDouble(20.0) :: Nil) :: JDouble(30.0) :: Nil)) :: Nil)) :: Nil))))
+    ("foo" within Circle((10, 20), 30)) must equal (MongoFieldFilter("foo", $within, MongoPrimitiveJObject(JObject(JField("$within", JObject(JField("$center",  JArray(JArray(JDouble(10.0) :: JDouble(20.0) :: Nil) :: JDouble(30.0) :: Nil)) :: Nil)) :: Nil))))
   }
   "builds $within for CenterSphere" in {
-    ("foo" within CenterSphere((10, 20), 0.3)) mustEqual (MongoFieldFilter("foo", $within, MongoPrimitiveJObject(JObject(JField("$within", JObject(JField("$centerSphere",  JArray(JArray(JDouble(10.0) :: JDouble(20.0) :: Nil) :: JDouble(0.3) :: Nil)) :: Nil)) :: Nil))))
+    ("foo" within CenterSphere((10, 20), 0.3)) must equal (MongoFieldFilter("foo", $within, MongoPrimitiveJObject(JObject(JField("$within", JObject(JField("$centerSphere",  JArray(JArray(JDouble(10.0) :: JDouble(20.0) :: Nil) :: JDouble(0.3) :: Nil)) :: Nil)) :: Nil))))
   }
   "builds $within for polygon" in {
-    ("foo" within Polygon((10, 20), (30, 40))) mustEqual (MongoFieldFilter("foo", $within, MongoPrimitiveJObject(JObject(JField("$within", JObject(JField("$polygon", JArray(JArray(JDouble(10.0) :: JDouble(20.0) :: Nil) :: JArray(JDouble(30.0) :: JDouble(40.0) :: Nil) :: Nil)) :: Nil)) :: Nil))))
+    ("foo" within Polygon((10, 20), (30, 40))) must equal (MongoFieldFilter("foo", $within, MongoPrimitiveJObject(JObject(JField("$within", JObject(JField("$polygon", JArray(JArray(JDouble(10.0) :: JDouble(20.0) :: Nil) :: JArray(JDouble(30.0) :: JDouble(40.0) :: Nil) :: Nil)) :: Nil)) :: Nil))))
   }
   "builds $where operation" in {
-    (evaluation("this.a > 3")) mustEqual (MongoFieldFilter(JPath.Identity, $where, MongoPrimitiveString("this.a > 3")))
+    (evaluation("this.a > 3")) must equal (MongoFieldFilter(JPath.Identity, $where, MongoPrimitiveString("this.a > 3")))
   }
 
 }
