44fe00b9043b5bc5b86f47b6634ac00e7eaa10c4
cheeseng <cheeseng@amaseng.com>
2012-07-24 16:36:53 +0800

    Rewritten EqFieldFilterEvaluatorSpec using ScalaTest.

--- mongo/src/test/scala/blueeyes/persistence/mongo/EqFieldFilterEvaluatorSpec.scala
+++ mongo/src/test/scala/blueeyes/persistence/mongo/EqFieldFilterEvaluatorSpec.scala
@@ -1,17 +1,17 @@
 package blueeyes.persistence.mongo
 
-import org.specs2.mutable.Specification
+import org.scalatest._
 import blueeyes.json.JsonAST._
 import Evaluators._
 
-class EqFieldFilterEvaluatorSpec  extends Specification {
+class EqFieldFilterEvaluatorSpec  extends WordSpec with MustMatchers {
   "returns true for the same JValues" in {
-    EqFieldFilterEvaluator(JString("foo"), JString("foo")) must be_==(true)
+    EqFieldFilterEvaluator(JString("foo"), JString("foo")) must equal (true)
   }
   "returns false for different JValues" in {
-    EqFieldFilterEvaluator(JString("bar"), JString("foo")) must be_==(false)
+    EqFieldFilterEvaluator(JString("bar"), JString("foo")) must equal (false)
   }
   "returns true if valie is JNothing and matching value is JNull" in {
-    EqFieldFilterEvaluator(JNothing, JNull) must be_==(true)
+    EqFieldFilterEvaluator(JNothing, JNull) must equal (true)
   }
 }
