0d9e50e3255d4ed2e654aa37560c1a6a6f1a95cb
cheeseng <cheeseng@amaseng.com>
2012-07-24 17:42:31 +0800

    Rewritten LtFieldFilterEvaluatorSpec using ScalaTest.

--- mongo/src/test/scala/blueeyes/persistence/mongo/LtFieldFilterEvaluatorSpec.scala
+++ mongo/src/test/scala/blueeyes/persistence/mongo/LtFieldFilterEvaluatorSpec.scala
@@ -1,38 +1,38 @@
 package blueeyes.persistence.mongo
 
-import org.specs2.mutable.Specification
+import org.scalatest._
 import blueeyes.json.JsonAST._
 import Evaluators._
 
-class LtFieldFilterEvaluatorSpec extends Specification {
+class LtFieldFilterEvaluatorSpec extends WordSpec with MustMatchers {
   "returns false when one string greater then another string" in {
-    LtFieldFilterEvaluator(JString("b"), JString("a")) must be_==(false)
+    LtFieldFilterEvaluator(JString("b"), JString("a")) must equal (false)
   }
   "returns true when one string less then another string" in {
-    LtFieldFilterEvaluator(JString("a"), JString("b")) must be_==(true)
+    LtFieldFilterEvaluator(JString("a"), JString("b")) must equal (true)
   }
   "returns false when one number greater then another number" in {
-    LtFieldFilterEvaluator(JInt(2), JInt(1)) must be_==(false)
+    LtFieldFilterEvaluator(JInt(2), JInt(1)) must equal (false)
   }
   "returns true when one number less then another number" in {
-    LtFieldFilterEvaluator(JInt(1), JInt(2)) must be_==(true)
+    LtFieldFilterEvaluator(JInt(1), JInt(2)) must equal (true)
   }
   "returns false when one double greater then another double" in {
-    LtFieldFilterEvaluator(JDouble(2.2), JDouble(1.1)) must be_==(false)
+    LtFieldFilterEvaluator(JDouble(2.2), JDouble(1.1)) must equal (false)
   }
   "returns true when one double less then another double" in {
-    LtFieldFilterEvaluator(JDouble(1.1), JDouble(2.2)) must be_==(true)
+    LtFieldFilterEvaluator(JDouble(1.1), JDouble(2.2)) must equal (true)
   }
   "returns false when one boolean greater then another boolean" in {
-    LtFieldFilterEvaluator(JBool(true), JBool(false)) must be_==(false)
+    LtFieldFilterEvaluator(JBool(true), JBool(false)) must equal (false)
   }
   "returns true when one boolean less then another boolean" in {
-    LtFieldFilterEvaluator(JBool(false), JBool(true)) must be_==(true)
+    LtFieldFilterEvaluator(JBool(false), JBool(true)) must equal (true)
   }
   "returns false when different object are compared" in {
-    LtFieldFilterEvaluator(JBool(false), JInt(1)) must be_==(false)
+    LtFieldFilterEvaluator(JBool(false), JInt(1)) must equal (false)
   }
   "returns false when objecta are the same" in {
-    LtFieldFilterEvaluator(JInt(1), JInt(1)) must be_==(false)
+    LtFieldFilterEvaluator(JInt(1), JInt(1)) must equal (false)
   }
 }
