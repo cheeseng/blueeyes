1b8d8f647bfad75d90cb4c8e55a8e582bf0be19e
cheeseng <cheeseng@amaseng.com>
2012-07-24 16:37:49 +0800

    Rewritten ExistFieldFilterEvaluatorSpec using ScalaTest.

--- mongo/src/test/scala/blueeyes/persistence/mongo/ExistFieldFilterEvaluatorSpec.scala
+++ mongo/src/test/scala/blueeyes/persistence/mongo/ExistFieldFilterEvaluatorSpec.scala
@@ -1,13 +1,13 @@
 package blueeyes.persistence.mongo
 
-import org.specs2.mutable.Specification
+import org.scalatest._
 import blueeyes.json.JsonAST._
 import Evaluators._
 
-class ExistFieldFilterEvaluatorSpec  extends Specification {
+class ExistFieldFilterEvaluatorSpec  extends WordSpec with MustMatchers {
 
   "always returns true" in {
-    ExistsFieldFilterEvaluator(JArray(JInt(2) :: JInt(3) :: Nil ), JBool(true)) must be_==(true)
-    ExistsFieldFilterEvaluator(JInt(4), JBool(true)) must be_==(true)
+    ExistsFieldFilterEvaluator(JArray(JInt(2) :: JInt(3) :: Nil ), JBool(true)) must equal (true)
+    ExistsFieldFilterEvaluator(JInt(4), JBool(true)) must equal (true)
   }
 }
