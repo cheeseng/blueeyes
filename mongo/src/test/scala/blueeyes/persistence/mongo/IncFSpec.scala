1a9c340f653a714b4a46fd9552f3bf94aba29a15
cheeseng <cheeseng@amaseng.com>
2012-07-24 17:11:05 +0800

    Rewritten IncFSpec using ScalaTest.

--- mongo/src/test/scala/blueeyes/persistence/mongo/IncFSpec.scala
+++ mongo/src/test/scala/blueeyes/persistence/mongo/IncFSpec.scala
@@ -1,13 +1,13 @@
 package blueeyes.persistence.mongo
 
-import org.specs2.mutable.Specification
+import org.scalatest._
 import UpdateFieldFunctions._
 
-class IncFSpec extends Specification{
+class IncFSpec extends WordSpec with MustMatchers {
   "fuse increases set update" in {
-    IncF("n", 2).fuseWith(SetF("n", 3)) mustEqual(Some(SetF("n", 5)))
+    IncF("n", 2).fuseWith(SetF("n", 3)) must equal (Some(SetF("n", 5)))
   }
   "fuse increases set inc update" in {
-    IncF("n", 2).fuseWith(IncF("n", 4)) mustEqual(Some(IncF("n", 6)))
+    IncF("n", 2).fuseWith(IncF("n", 4)) must equal (Some(IncF("n", 6)))
   }
 }
