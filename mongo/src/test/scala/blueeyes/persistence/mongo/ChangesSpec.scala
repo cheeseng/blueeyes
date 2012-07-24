190b0235d5dcb7373963b0e35c599d746f51bd83
cheeseng <cheeseng@amaseng.com>
2012-07-24 13:46:32 +0800

    Rewritten ChangesSpec using ScalaTest.

--- mongo/src/test/scala/blueeyes/persistence/mongo/ChangesSpec.scala
+++ mongo/src/test/scala/blueeyes/persistence/mongo/ChangesSpec.scala
@@ -1,28 +1,28 @@
 package blueeyes.persistence.mongo
 
-import org.specs2.mutable.Specification
+import org.scalatest._
 import blueeyes.json.JPath
 import blueeyes.persistence.mongo.Changes.Change1
 
 
-class ChangesSpec extends Specification {
+class ChangesSpec extends WordSpec with MustMatchers {
   "Changes.compose" should{
     "leave changes with diffrent paths" in{
-      Changes.compose(List(ChangeImpl(JPath("bar"), 2, false)), List(ChangeImpl(JPath("foo"), 2, false))) mustEqual(List(ChangeImpl(JPath("bar"), 2, false), ChangeImpl(JPath("foo"), 2, false)))
+      Changes.compose(List(ChangeImpl(JPath("bar"), 2, false)), List(ChangeImpl(JPath("foo"), 2, false))) must equal (List(ChangeImpl(JPath("bar"), 2, false), ChangeImpl(JPath("foo"), 2, false)))
     }
     "fuse changes with the same paths" in{
-      Changes.compose(List(ChangeImpl(JPath("foo"), 2, true)), List(ChangeImpl(JPath("foo"), 1, true))) mustEqual(List(ChangeImpl(JPath("foo"), 1, true)))
+      Changes.compose(List(ChangeImpl(JPath("foo"), 2, true)), List(ChangeImpl(JPath("foo"), 1, true))) must equal (List(ChangeImpl(JPath("foo"), 1, true)))
     }
     "fuse several changes" in{
       val listSet = Changes.compose(List(ChangeImpl(JPath("foo"), 2, true), ChangeImpl(JPath("bar"), 2, true)), List(ChangeImpl(JPath("foo"), 1, true), ChangeImpl(JPath("bar"), 1, true)))
-      listSet mustEqual(List(ChangeImpl(JPath("foo"), 1, true), ChangeImpl(JPath("bar"), 1, true)))
+      listSet must equal (List(ChangeImpl(JPath("foo"), 1, true), ChangeImpl(JPath("bar"), 1, true)))
     }
     "leave changes with the same paths which cannot be fused" in{
-      Changes.compose(List(ChangeImpl(JPath("foo"), 2, false)), List(ChangeImpl(JPath("foo"), 1, false))) mustEqual(List(ChangeImpl(JPath("foo"), 1, false), ChangeImpl(JPath("foo"), 2, false)))
+      Changes.compose(List(ChangeImpl(JPath("foo"), 2, false)), List(ChangeImpl(JPath("foo"), 1, false))) must equal (List(ChangeImpl(JPath("foo"), 1, false), ChangeImpl(JPath("foo"), 2, false)))
     }
     "fuse only changes  which can be fused" in{
       val listSet = Changes.compose(List(ChangeImpl(JPath("bar"), 2, true), ChangeImpl(JPath("foo"), 2, true), ChangeImpl(JPath("foo"), 2, false)), List(ChangeImpl(JPath("foo"), 1, true)))
-      listSet mustEqual(List(ChangeImpl(JPath("bar"), 2, true), ChangeImpl(JPath("foo"), 2, false), ChangeImpl(JPath("foo"), 1, true)))
+      listSet must equal (List(ChangeImpl(JPath("bar"), 2, true), ChangeImpl(JPath("foo"), 2, false), ChangeImpl(JPath("foo"), 1, true)))
     }
   }
 }
