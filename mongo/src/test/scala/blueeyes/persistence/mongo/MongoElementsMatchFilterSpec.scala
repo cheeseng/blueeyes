f7d400465dd6d55eac5aa43db38dc07554d3fe5f
cheeseng <cheeseng@amaseng.com>
2012-07-24 18:16:27 +0800

    Rewritten MongoElementsMatchFilterSpec using ScalaTest.

--- mongo/src/test/scala/blueeyes/persistence/mongo/MongoElementsMatchFilterSpec.scala
+++ mongo/src/test/scala/blueeyes/persistence/mongo/MongoElementsMatchFilterSpec.scala
@@ -1,20 +1,20 @@
 package blueeyes.persistence.mongo
 
-import org.specs2.mutable.Specification
+import org.scalatest._
 import MongoFilterOperators._
 import blueeyes.json.JsonAST._
 import blueeyes.json.JPathImplicits._
 import blueeyes.json._
 
-class MongoElementsMatchFilterSpec extends Specification{
+class MongoElementsMatchFilterSpec extends WordSpec with MustMatchers {
   private val filter1    = MongoFilterBuilder(JPath("foo")).>(MongoPrimitiveInt(1))
   private val filter2    = MongoFilterBuilder(JPath("bar")).<(MongoPrimitiveInt(5))
   private val andFilter  = filter1 && filter2
 
   "create valid json for or filter" in {
-    (andFilter.elemMatch("name")).filter mustEqual (JsonParser.parse(""" {"name": {"$elemMatch" : {"foo": {"$gt": 1}, "bar": {"$lt": 5}} }} """))
+    (andFilter.elemMatch("name")).filter must equal (JsonParser.parse(""" {"name": {"$elemMatch" : {"foo": {"$gt": 1}, "bar": {"$lt": 5}} }} """))
   }
   "create valid json for or filter when path is empty" in {
-    (andFilter.elemMatch("")).filter mustEqual (JsonParser.parse(""" {"$elemMatch" : {"foo": {"$gt": 1}, "bar": {"$lt": 5}} } """))
+    (andFilter.elemMatch("")).filter must equal (JsonParser.parse(""" {"$elemMatch" : {"foo": {"$gt": 1}, "bar": {"$lt": 5}} } """))
   }
 }
