db7ddc7dd28df3bce32e368a19066ce1fb812150
cheeseng <cheeseng@amaseng.com>
2012-07-24 18:46:22 +0800

    Rewritten MongoMapReduceQuerySpec using ScalaTest.

--- mongo/src/test/scala/blueeyes/persistence/mongo/MongoMapReduceQuerySpec.scala
+++ mongo/src/test/scala/blueeyes/persistence/mongo/MongoMapReduceQuerySpec.scala
@@ -1,20 +1,20 @@
 package blueeyes.persistence.mongo
 
-import org.specs2.mutable.Specification
+import org.scalatest._
 import MongoQueryBuilder._
 import MongoFilterBuilder._
 import MongoFilterOperators._
 import blueeyes.json.JPathImplicits._
 import blueeyes.json.JPath
 
-class MongoMapReduceQuerySpec extends Specification{
+class MongoMapReduceQuerySpec extends WordSpec with MustMatchers {
   private val query = mapReduce("foo", "bar").from(MongoCollectionReference("collection"))
 
   "'where' method sets new filter" in {
-    query.where("name" === "Joe") mustEqual (MongoMapReduceQuery("foo", "bar",  "collection", None, Some(MongoFieldFilter("name", $eq, "Joe"))))
+    query.where("name" === "Joe") must equal (MongoMapReduceQuery("foo", "bar",  "collection", None, Some(MongoFieldFilter("name", $eq, "Joe"))))
   }
 
   "'on' method set output collection name" in {
-    query.into("output") mustEqual (MongoMapReduceQuery("foo", "bar",  "collection", Some("output"), None))
+    query.into("output") must equal (MongoMapReduceQuery("foo", "bar",  "collection", Some("output"), None))
   }
 }
