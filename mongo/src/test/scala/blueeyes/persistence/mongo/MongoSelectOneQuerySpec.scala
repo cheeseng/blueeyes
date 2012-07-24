55565101ff33cba7f59e78688ce6b2d2acccf6ca
cheeseng <cheeseng@amaseng.com>
2012-07-24 23:57:09 +0800

    Rewritten MongoSelectionOneQuerySpec using ScalaTest.

--- mongo/src/test/scala/blueeyes/persistence/mongo/MongoSelectOneQuerySpec.scala
+++ mongo/src/test/scala/blueeyes/persistence/mongo/MongoSelectOneQuerySpec.scala
@@ -1,6 +1,6 @@
 package blueeyes.persistence.mongo
 
-import org.specs2.mutable.Specification
+import org.scalatest._
 import MongoQueryBuilder._
 import MongoFilterBuilder._
 import MongoFilterOperators._
@@ -8,14 +8,14 @@ import blueeyes.json.JPathImplicits._
 import blueeyes.json.JsonAST._
 import blueeyes.json.JPath
 
-class MongoSelectOneQuerySpec extends Specification{
+class MongoSelectOneQuerySpec extends WordSpec with MustMatchers {
   private val query = selectOne("foo", "bar").from(MongoCollectionReference("collection"))
 
   "'where' method sets new filter" in {
-    query.where("name".jpath === "Joe") mustEqual (MongoSelectOneQuery(MongoSelection(Set(JPath("foo"), JPath("bar"))), "collection", Some(MongoFieldFilter("name", $eq, "Joe"))))
+    query.where("name".jpath === "Joe") must equal (MongoSelectOneQuery(MongoSelection(Set(JPath("foo"), JPath("bar"))), "collection", Some(MongoFieldFilter("name", $eq, "Joe"))))
   }
 
   "'sortBy' method sets new sort" in {
-    query.sortBy("name".jpath << ) mustEqual (MongoSelectOneQuery(MongoSelection(Set(JPath("foo"), JPath("bar"))), "collection", None, Some(MongoSort(JPath("name"), MongoSortOrderDescending))))
+    query.sortBy("name".jpath << ) must equal (MongoSelectOneQuery(MongoSelection(Set(JPath("foo"), JPath("bar"))), "collection", None, Some(MongoSort(JPath("name"), MongoSortOrderDescending))))
   }
 }
