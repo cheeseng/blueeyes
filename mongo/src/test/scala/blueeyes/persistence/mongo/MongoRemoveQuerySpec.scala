6e54b5941b06cb8ebd2a021899be5eca43c0b649
cheeseng <cheeseng@amaseng.com>
2012-07-24 23:56:40 +0800

    Rewritten MongoRemoveQuerySpec using ScalaTest.

--- mongo/src/test/scala/blueeyes/persistence/mongo/MongoRemoveQuerySpec.scala
+++ mongo/src/test/scala/blueeyes/persistence/mongo/MongoRemoveQuerySpec.scala
@@ -1,13 +1,13 @@
 package blueeyes.persistence.mongo
 
-import org.specs2.mutable.Specification
+import org.scalatest._
 import MongoQueryBuilder._
 import MongoFilterOperators._
 
-class MongoRemoveQuerySpec extends Specification{
+class MongoRemoveQuerySpec extends WordSpec with MustMatchers {
 
   "'where' method sets new filter" in {
     val query = remove.from("collection")
-    query.where("name" === "Joe") mustEqual ( MongoRemoveQuery("collection", Some(MongoFieldFilter("name", $eq, "Joe"))) )
+    query.where("name" === "Joe") must equal ( MongoRemoveQuery("collection", Some(MongoFieldFilter("name", $eq, "Joe"))) )
   }
 }
