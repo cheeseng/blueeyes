f924d02ad334bf25fc32feeca7979b44eab6a2c0
cheeseng <cheeseng@amaseng.com>
2012-07-24 16:37:20 +0800

    Rewritten EvaluatorsSpec using ScalaTest.

--- mongo/src/test/scala/blueeyes/persistence/mongo/EvaluatorsSpec.scala
+++ mongo/src/test/scala/blueeyes/persistence/mongo/EvaluatorsSpec.scala
@@ -1,15 +1,15 @@
 package blueeyes.persistence.mongo
 
-import org.specs2.mutable.Specification
+import org.scalatest._
 
-class EvaluatorsSpec extends Specification{
+class EvaluatorsSpec extends WordSpec with MustMatchers {
   private val palygon = Array((-2.0, 3.0), (-2.0, 1.0), (-1.0, 1.0), (-1.0, 3.0))
   "inPolygon" should{
     "does not contain outer point" in {
-      Evaluators.inPolygon((2.0, 3.0), palygon) must be_==(false)
+      Evaluators.inPolygon((2.0, 3.0), palygon) must equal (false)
     }
     "contains inner point" in {
-      Evaluators.inPolygon((-1.0, 2.0), palygon) must be_==(true)
+      Evaluators.inPolygon((-1.0, 2.0), palygon) must equal (true)
     }
   }
 }
