fb525e53bc780f2b9733d024a4b870818887301b
cheeseng <cheeseng@amaseng.com>
2012-07-24 17:43:27 +0800

    Rewritten ModFieldFilterEvaluatorSpec using ScalaTest.

--- mongo/src/test/scala/blueeyes/persistence/mongo/ModFieldFilterEvaluatorSpec.scala
+++ mongo/src/test/scala/blueeyes/persistence/mongo/ModFieldFilterEvaluatorSpec.scala
@@ -1,17 +1,17 @@
 package blueeyes.persistence.mongo
 
-import org.specs2.mutable.Specification
+import org.scalatest._
 import blueeyes.json.JsonAST._
 import Evaluators._
 
-class ModFieldFilterEvaluatorSpec  extends Specification {
+class ModFieldFilterEvaluatorSpec  extends WordSpec with MustMatchers {
   "returns true when int modular devision is true" in {
-    ModFieldFilterEvaluator(JInt(11), JArray(JInt(10) :: JInt(1) :: Nil )) must be_==(true)
+    ModFieldFilterEvaluator(JInt(11), JArray(JInt(10) :: JInt(1) :: Nil )) must equal (true)
   }
   "returns true when double modular devision is true" in {
-    ModFieldFilterEvaluator(JDouble(11.0), JArray(JInt(10) :: JInt(1) :: Nil )) must be_==(true)
+    ModFieldFilterEvaluator(JDouble(11.0), JArray(JInt(10) :: JInt(1) :: Nil )) must equal (true)
   }
   "returns false when int modular devision is false" in {
-    ModFieldFilterEvaluator(JInt(21), JArray(JInt(10) :: JInt(2) :: Nil )) must be_==(false)
+    ModFieldFilterEvaluator(JInt(21), JArray(JInt(10) :: JInt(2) :: Nil )) must equal (false)
   }
 }
