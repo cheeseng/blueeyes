8c81188b475e341f24e270cf345b9fbfaece073b
cheeseng <cheeseng@amaseng.com>
2012-07-24 18:45:37 +0800

    Rewritten MongoGroupQuerySpec using ScalaTest.

--- mongo/src/test/scala/blueeyes/persistence/mongo/MongoGroupQuerySpec.scala
+++ mongo/src/test/scala/blueeyes/persistence/mongo/MongoGroupQuerySpec.scala
@@ -1,6 +1,6 @@
 package blueeyes.persistence.mongo
 
-import org.specs2.mutable.Specification
+import org.scalatest._
 import MongoQueryBuilder._
 import MongoFilterBuilder._
 import MongoFilterOperators._
@@ -8,10 +8,10 @@ import blueeyes.json.JPath
 import blueeyes.json.JPathImplicits._
 import blueeyes.json.JsonAST.JObject
 
-class MongoGroupQuerySpec extends Specification{
+class MongoGroupQuerySpec extends WordSpec with MustMatchers {
   private val query = group(JObject(Nil), "dummy", "foo", "bar").from(MongoCollectionReference("collection"))
 
   "'where' method sets new filter" in {
-    query.where("name" === "Joe") mustEqual (MongoGroupQuery(MongoSelection(Set(JPath("foo"), JPath("bar"))), "collection", JObject(Nil), "dummy", Some(MongoFieldFilter("name", $eq, "Joe"))))
+    query.where("name" === "Joe") must equal (MongoGroupQuery(MongoSelection(Set(JPath("foo"), JPath("bar"))), "collection", JObject(Nil), "dummy", Some(MongoFieldFilter("name", $eq, "Joe"))))
   }
 }
